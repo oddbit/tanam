38a2bea0a98d32ee996f052ffe209625f7ed8f47
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2018-05-21 14:55:59 +0800

    add profile route

--- src/admin_client/src/router/index.js
+++ src/admin_client/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Profile from '@/views/Profile/Profile';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
     },
   ],
 });
