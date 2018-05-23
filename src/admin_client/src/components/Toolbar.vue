aa7f25dc5f7b51fd2f30e45e8fea14837ea19b3d
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2018-05-23 14:27:04 +0800

    implement menu for profile

--- src/admin_client/src/components/Toolbar.vue
+++ src/admin_client/src/components/Toolbar.vue
@@ -22,15 +22,40 @@
     <v-btn icon>
       <v-icon>notifications</v-icon>
     </v-btn>
-    <span
-      class="toolbar-avatar"
-      @click="$router.push('/profile')">
-      <img src="@/assets/images/gravatar.jpg">
-    </span>
+    <v-menu 
+      class="ml-2 mr-4"
+      left 
+      bottom
+      offset-y
+      nudge-top="-10"
+      max-width="85%"
+      light>
+      <span
+        slot="activator"
+        class="toolbar-avatar">
+        <img src="@/assets/images/gravatar.jpg">
+      </span>
+      <v-layout class="menu-layout-wrapper" column>
+        <v-layout class="pa-3">
+          <span class="img-wrapper"><img src="@/assets/images/gravatar.jpg"></span>
+          <div class="ml-3 text-xs-left user">
+            <h4>I Made Ananda Widiprabawa</h4>
+            <p>ananda.widiprabawa@gmail.com</p>
+          </div>
+        </v-layout>
+        <v-layout class="px-2 py-1 grey lighten-3" justify-space-between>
+          <v-btn @click="$router.push('/profile')" color="primary">My Profile</v-btn>
+          <v-btn @click="handleLogout">Logout</v-btn>
+        </v-layout>
+      </v-layout>
+    </v-menu>
   </v-toolbar>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+import { LOGOUT } from '@/store/types';
+
 export default {
   computed: {
     isProfileRoute() {
@@ -38,6 +63,9 @@ export default {
     }
   },
   methods: {
+    ...mapActions({
+      handleLogout: LOGOUT
+    }),
     handleClickMenu() {
       this.$store.commit('drawer/toggleDrawer', true);
     }
@@ -58,11 +86,40 @@ export default {
   display: flex;
   align-items: center;
   cursor: pointer;
-  margin-left: 8px;
   img {
     max-width: 100%;
     border-radius: 100%;
     object-fit: cover;
+  }
+}
+.menu-layout-wrapper {
+  background: #fff;
+  min-width: 250px;
+  .img-wrapper {
+    width: 80px;
+    display: flex;
+    align-items: center;
+    img {
+      max-width: 100%;
+      border-radius: 100%;
+      object-fit: cover;
+    }
+  }
+  .user {
+    min-width: 0;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    h4,
+    p {
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+    p {
+      margin-bottom: 0;
+    }
   }
 }
 </style>
