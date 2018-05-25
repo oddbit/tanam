2384f7de9ec94ac25b56f99476105e9d9a79df8b
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2018-05-25 12:30:57 +0800

    add account settings section

---
+++ src/admin_client/src/components/Profile/Account.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card>
+    <v-card-title class="card-title px-4">Account</v-card-title>
+    
+    <v-container grid-list-xl>
+      <v-layout>
+        <v-flex>
+          <v-form>
+            <v-text-field name="username" label="Username" v-model="username" />
+            <v-text-field 
+              name="email" 
+              label="Email" 
+              :value="email" 
+              disabled />
+
+            <v-layout>
+              <v-flex d-flex justify-end>
+                <v-btn color="primary" class="btn-save ma-0">Save Account</v-btn>
+              </v-flex>
+            </v-layout>
+
+          </v-form>        
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    username: 'anandaprabawa',
+    email: 'ananda@oddbit.id'
+  })
+};
+</script>
+
+
+<style lang="scss" scoped>
+.card-title {
+  border-bottom: 1px solid #eee;
+  font-weight: 500;
+}
+.btn-save {
+  flex: 0 1 auto !important;
+}
+</style>
