5e02a5be593c9d06853eb3faae6e94d05f056edd
praburangki <praburangki@gmail.com>
2018-06-12 15:15:40 +0800

    only allow google login for now

--- src/admin_client/src/views/Login.vue
+++ src/admin_client/src/views/Login.vue
@@ -16,13 +16,7 @@ import { firebaseUI } from '@/utils/firebase';
 
 const uiConfig = {
   signInSuccessUrl: '/',
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   tosUrl: '/#tos',
   signInFlow: 'popup'
 };
