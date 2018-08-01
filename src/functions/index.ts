5a1052ce992942f03cc0be556d10103a4900d6b7
praburangki <praburangki@gmail.com>
2018-08-01 14:05:18 +0800

    [TNM-70] Abstract default configuration

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,34 +1,32 @@
-import * as functions from 'firebase-functions';
 import * as express from 'express';
 import { Nuxt } from 'nuxt';
+import TanamConfig, { BaseConfig } from './config';
 
-const themeDir = './node_modules/tanam/dist/theme';
-const adminDir = './node_modules/tanam/dist/admin_client';
+export class App {
+  public app: express.Application;
+  private nuxt: any;
+  private _tanamConfig: TanamConfig;
 
-const config = {
-  dev: false,
-  buildDir: themeDir,
-  build: {
-    publicPath: '/assets/'
+  constructor(tanamConfig: BaseConfig = {}) {
+    this._tanamConfig = new TanamConfig(tanamConfig);
+
+    const { configuration } = this._tanamConfig;
+    const { adminUrl, adminDir } = configuration;
+
+    this.nuxt = new Nuxt(configuration.nuxtConfig);
+
+    this.app = express();
+
+    this.app.use(`/${adminUrl}/`, express.static(adminDir));
+    this.app.use(`/${adminUrl}/**`, (req: express.Request, res: express.Response) => {
+      res.status(200).sendFile('index.html', { root: adminDir });
+    });
+
+    this.app.get('**', this.handleThemeRequest.bind(this));
   }
-};
 
-const nuxt = new Nuxt(config);
-
-const handleThemeRequest = (req, res) => {
-  res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
-  return nuxt.render(req, res);
+  handleThemeRequest(req: express.Request, res: express.Response) {
+    res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
+    return this.nuxt.render(req, res);
+  }
 }
-
-const adminPage = adminDir + '/';
-
-const app = express();
-
-app.use('/admin/', express.static(adminPage));
-app.use('/admin/**', (req, res) => {
-  res.status(200).sendFile('index.html', { root: adminPage });
-});
-
-app.get('**', handleThemeRequest);
-
-export const theme = functions.https.onRequest(app);
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,4 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-export interface Configuration { }
-export function configure(configuration: Configuration) {
-  console.log("Hello World");
-};
-
 export * from './functions';
