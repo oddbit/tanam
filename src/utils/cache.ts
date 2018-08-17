70d1525f6241452e04c124238eacf263905254c5
Dennis Alund <dennis.alund@gmail.com>
2018-08-17 23:16:53 +0800

    Adding a "heat cache" function
    For triggering a build of our newly created document

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -13,3 +13,17 @@ export function purgeCache(host: string, path: string) {
         https.request(opts, resolve);
     });
 }
+
+export function heatCache(host: string, path: string) {
+    console.log(`Heating cache: ${host}${path}`);
+    const opts = {
+        hostname: host,
+        port: 443,
+        path: path,
+        method: 'GET'
+    };
+
+    return new Promise((resolve, _) => {
+        https.get(opts, resolve);
+    });
+}
