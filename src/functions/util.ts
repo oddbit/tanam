bdd0c196bcb430116f7f26e73426dc1e352a3c90
Dennis Alund <dennis.alund@gmail.com>
2018-08-17 12:52:16 +0800

    Adding a path encoder
    In order to store a master record of all path -> document mappings

---
+++ src/functions/util.ts
@@ -0,0 +1,16 @@
+function encodePath(path) {
+    function encodeAndAsssemble(str, splitter) {
+        return str
+            .split(splitter)
+            .map((sub) => {
+                return Buffer.from(sub).toString('base64');
+            })
+            .join('/');
+    }
+
+    return path
+        .split('?')
+        .map((part, index) => {
+            return index === 0 ? encodeAndAsssemble(part, '/') : encodeAndAsssemble(part, '&');
+        }).join('/');
+}
