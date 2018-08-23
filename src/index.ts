522bc8ce8626dc878698f641cbbb185993dbdd92
praburangki <praburangki@gmail.com>
2018-08-23 16:18:08 +0800

    setup firebase config in tanam express

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import * as admin from 'firebase-admin';
 import * as express from 'express';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as handlebars from 'handlebars';
 import * as handlers from './handlers';
 
 if (admin.apps.length === 0) {
   admin.initializeApp();
 }
 
+const adminClientDir = path.join(__dirname, 'admin_client');
+
 export * from './cache';
 export const app = express();
 export function initializeApp(tanamConfig: TanamConfig = {}) {
   const appConfig = { ...tanamDefaultAppConfig, ...(tanamConfig || {}) };
 
-  app.use(`/${appConfig.adminUrl}/`, express.static('./admin_client'));
+  injectFirebaseConfig(tanamConfig.firebaseConfig);
+
+  app.use(`/${appConfig.adminUrl}/`, express.static(adminClientDir));
   app.use(`/${appConfig.adminUrl}/**`, (req: express.Request, res: express.Response) => {
-    res.status(200).sendFile('index.html', { root: './admin_client' });
+    res.status(200).sendFile('index.html', { root: adminClientDir });
   });
 
   app.get('/manifest.json', handlers.handleWebManifestReq);
@@ -22,8 +29,17 @@ export function initializeApp(tanamConfig: TanamConfig = {}) {
 
 export interface TanamConfig {
   adminUrl?: string;
+  firebaseConfig?: any;
 }
 
 const tanamDefaultAppConfig: TanamConfig = {
   adminUrl: 'admin'
 };
+
+function injectFirebaseConfig(firebaseConfig) {
+  const indexFileName = path.join(adminClientDir, '/index.html');
+  const indexFile = fs.readFileSync(indexFileName, 'utf8');
+  firebaseConfig['stringify'] = JSON.stringify(firebaseConfig);
+  const compiledHtml = handlebars.compile(indexFile)({ fbConfig: firebaseConfig });
+  fs.writeFileSync(indexFileName, compiledHtml);
+}
