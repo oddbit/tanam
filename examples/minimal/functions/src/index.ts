bb1315ee79d2ca01a6f0d90b57e47aba7fc88e74
Dennis Alund <dennis.alund@gmail.com>
2018-08-23 22:57:59 +0800

    Updating example

--- examples/minimal/functions/src/index.ts
+++ examples/minimal/functions/src/index.ts
@@ -1,7 +1,7 @@
-import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import * as tanam from 'tanam';
 
+admin.initializeApp();
 tanam.initializeApp();
 
-export const tanam_app = functions.https.onRequest(tanam.app);
-export const tanamOnDocWriteUpdateCache = functions.firestore.document(tanam.firebaseFunctionsDocPath).onWrite(tanam.onDocWriteUpdateCache);
+export * from 'tanam';
