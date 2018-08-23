8567918e9710369cdb01c28175ae73ff5435e098
Dennis Alund <dennis.alund@gmail.com>
2018-08-23 19:32:11 +0800

    Can't use file system on cloud functions

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,15 @@
 import * as admin from 'firebase-admin';
 import * as express from 'express';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as handlebars from 'handlebars';
 import * as handlers from './handlers';
 
 if (admin.apps.length === 0) {
   admin.initializeApp();
 }
 
-const adminClientDir = path.join(__dirname, 'admin_client');
-
 export * from './cache';
 export const app = express();
 export function initializeApp(tanamConfig: TanamConfig = {}) {
   const appConfig = { ...tanamDefaultAppConfig, ...(tanamConfig || {}) };
-
-  injectFirebaseConfig(tanamConfig.firebaseConfig);
 
   app.use(`/${appConfig.adminUrl}/`, express.static(adminClientDir));
   app.use(`/${appConfig.adminUrl}/**`, (req: express.Request, res: express.Response) => {
@@ -35,11 +28,3 @@ export interface TanamConfig {
 const tanamDefaultAppConfig: TanamConfig = {
   adminUrl: 'admin'
 };
-
-function injectFirebaseConfig(firebaseConfig) {
-  const indexFileName = path.join(adminClientDir, '/index.html');
-  const indexFile = fs.readFileSync(indexFileName, 'utf8');
-  firebaseConfig['stringify'] = JSON.stringify(firebaseConfig);
-  const compiledHtml = handlebars.compile(indexFile)({ fbConfig: firebaseConfig });
-  fs.writeFileSync(indexFileName, compiledHtml);
-}
