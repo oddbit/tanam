fcb0ecea08eef1da611d7d98ba40c2f8610d4024
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2018-09-20 12:30:20 +0800

    [TNM-18] refactor MainDrawer (fetching content types)

--- src/admin_client/src/components/Drawer/MainDrawer.vue
+++ src/admin_client/src/components/Drawer/MainDrawer.vue
@@ -10,6 +10,19 @@
     </v-list>
 
     <v-list
+      subheader>
+      <v-subheader>CONTENT TYPES</v-subheader>      
+      <v-list-tile
+        ripple
+        v-for="type in contentTypes"
+        :key="type.title"
+        :to="{name: 'contentType', params: {link: type.permalink}}">
+        <v-list-tile-action><v-icon>{{ type.icon }}</v-icon></v-list-tile-action>
+        <v-list-tile-title>{{ type.title }}</v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+
+    <v-list
       subheader
       v-for="mainList in mainLists"
       :key="`mainList-${mainList.subheader}`">
@@ -24,25 +37,13 @@
       </v-list-tile>
     </v-list>
 
-    <v-list
-      subheader>
-      <v-subheader>CONTENT TYPES</v-subheader>      
-      <v-list-tile
-        ripple
-        v-for="(contentType, index) in dynamicContentTypes"
-        :key="index"
-        :to="{name: 'contentType', params: {link: `${contentType.permalink}`}}">
-        <v-list-tile-action><v-icon>{{contentType.icon}}</v-icon></v-list-tile-action>
-        <v-list-tile-title>{{contentType.title}}</v-list-tile-title>
-      </v-list-tile>
-    </v-list>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapGetters } from 'vuex';
 import { navDrawerLists } from '@/config/navDrawerLists';
-import { TEMPLATE_CONTENTTYPES } from '@/store/types';
+import { CONTENT_TYPE_NAMES } from '@/store/types';
 
 export default {
   data: () => ({
@@ -58,28 +59,17 @@ export default {
     ],
     navDrawerLists
   }),
-  mounted() {
-    this.$store.dispatch(TEMPLATE_CONTENTTYPES);
-  },
-  // filters: {
-  //   capitalize (title) {
-  //     return title.charAt(0).toUpperCase() + title.slice(1)
-  //   }
-  // },
-  computed: {
-    ...mapState({
-      contentTypes: state => state.templates.contentTypes
-    }),
-    dynamicContentTypes () {
-      const contentTypes = this.$store.state.templates.contentTypes
-      var a = Object.keys(contentTypes).map(key => ({
-        title: contentTypes[key].title,
-        icon: contentTypes[key].icon,
-        permalink: contentTypes[key].permalink
-      }))
-      console.log(a)
-      return a
+  async mounted() {
+    try {
+      await this.$store.dispatch(CONTENT_TYPE_NAMES);
+    } catch (error) {
+      throw error;
     }
+  },
+  computed: {
+    ...mapGetters({
+      contentTypes: [CONTENT_TYPE_NAMES]
+    })
   }
 };
 </script>
