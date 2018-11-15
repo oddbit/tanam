7f0027124d0de242a725b215a3008beaf6c89bfe
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2018-11-15 10:41:40 +0800

    add HeaderSection shared component

---
+++ src/admin_client/src/components/Shared/HeaderSection.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-layout 
+    row 
+    justify-space-between 
+    align-center>
+    <div class="wrapper">
+      <h1>{{ title }}</h1>
+      <p>{{ description }}</p>
+    </div>
+    <slot name="actions" />
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Section'
+    },
+    description: {
+      type: String,
+      default: 'Section description goes here'
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  h1 {
+    font-weight: 600;
+    font-size: 2rem;
+    color: #444;
+  }
+  p {
+    color: #555;
+  }
+}
+</style>
