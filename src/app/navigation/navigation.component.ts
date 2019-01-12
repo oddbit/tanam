07cae6c8f84b71dfde1ec6a5d56bd93d8e3aa452
Dennis Alund <dennis.alund@gmail.com>
2019-01-12 10:35:59 +0800

    Removing unused import

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { AngularFirestore } from '@angular/fire/firestore';
 import { ContentTypeService } from '../content-type/content-type.service';
 
 @Component({
