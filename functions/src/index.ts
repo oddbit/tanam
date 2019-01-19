c72e8c3e81e3ccb3789ece82b457c778f35bcf7f
Dennis Alund <dennis.alund@gmail.com>
2019-01-19 23:47:25 +0800

    Preparing for merge

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,2 +1,14 @@
+import * as functions from 'firebase-functions';
 
+const server = require(`./tanam-webpack/server`);
+
+export interface TanamConfig {
+    firebaseApp: any;
+}
+
+export function initializeApp(config: TanamConfig) {
+    server.initializeApp(config);
+}
+
+export const tanam = functions.https.onRequest(server.app);
 export * from './users';
