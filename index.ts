d137b10d9fe4240a0f37b098e5a2eee82c2bdda3
Dennis Alund <dennis.alund@gmail.com>
2019-01-21 12:00:44 +0800

    Configuring SSR with Firebase support

--- index.ts
+++ index.ts
@@ -2,14 +2,26 @@ import 'zone.js/dist/zone-node';
 import { enableProdMode } from '@angular/core';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import * as express from 'express';
 import { join } from 'path';
 
+// Required for Firebase
+(global as any).WebSocket = require('ws');
+(global as any).XMLHttpRequest = require('xhr2');
+
 enableProdMode();
 
-const app = express();
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist');
+const expressApp = express();
+
+if (admin.apps.length === 0) {
+    admin.initializeApp();
+}
+
+export const app = functions.https.onRequest(expressApp);
+
+const DIST_FOLDER = process.env.FUNCTION_NAME ? __dirname : join(__dirname, 'dist');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./server/main');
@@ -26,7 +38,7 @@ const appConfig = process.env.FIREBASE_CONFIG || {
     },
 };
 
-app.engine('html', ngExpressEngine({
+expressApp.engine('html', ngExpressEngine({
     bootstrap: AppServerModuleNgFactory,
     providers: [
         provideModuleMap(LAZY_MODULE_MAP),
@@ -37,17 +49,10 @@ app.engine('html', ngExpressEngine({
     ]
 }));
 
-app.set('view engine', 'html');
-app.set('views', join(DIST_FOLDER, 'browser'));
-
-// Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
-
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
+expressApp.set('view engine', 'html');
+expressApp.set('views', join(DIST_FOLDER, 'browser'));
+expressApp.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
+expressApp.get('*', (req, res) => {
+    // Render with universal engine
     res.render('index', { req });
-});
-
-app.listen(PORT, () => {
-    console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
