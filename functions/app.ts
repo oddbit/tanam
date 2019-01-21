d34cbe006397c21d483b9b49b56da20057ee8746
Dennis Alund <dennis.alund@gmail.com>
2019-01-21 10:06:16 +0800

    Adding functions

--- functions/app.ts
+++ functions/app.ts
@@ -1,5 +1,11 @@
+
 import * as functions from 'firebase-functions';
 
-export const app = functions.https.onRequest((req, res) => {
-    res.send('ok');
+export const app = functions.https.onRequest((request, response) => {
+    console.log('*****************************************************************');
+    const server = require(`./tanam-webpack/server`);
+    if (!server) {
+        throw new Error('Tanam was not properly configured. Make sure that the webpacked server is included.');
+    }
+    server.app(request, response);
 });
