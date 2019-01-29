e35287bcb00a28137adb85c4221b7e403f47fdf5
Deta Utama <detautama11@gmail.com>
2019-01-29 13:26:37 +0800

    Pass isExpand parameter to app-navigation-list-item

--- admin/src/app/admin/navigation/navigation-list-item/navigation-list-item.component.ts
+++ admin/src/app/admin/navigation/navigation-list-item/navigation-list-item.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class NavigationListItemComponent implements OnInit {
   @Input() icon: string;
+  @Input() isExpanded: string;
   @Input() title: string;
   @Input() routerLink: string;
 
