3066110c79e8c3eabeb67b94a4e4a30d6e8d2b4d
Dennis Alund <dennis.alund@gmail.com>
2019-01-29 14:04:41 +0800

    Adding app and lib

--- admin/projects/core/src/lib/core.service.spec.ts
+++ admin/projects/core/src/lib/core.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserPrefsService } from './user-prefs.service';
+import { CoreService } from './core.service';
 
-describe('UserPrefsService', () => {
+describe('CoreService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: UserPrefsService = TestBed.get(UserPrefsService);
+    const service: CoreService = TestBed.get(CoreService);
     expect(service).toBeTruthy();
   });
 });
