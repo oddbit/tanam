03faf325cb1f0af0e2ba5e8626c48606176e1662
Deta Utama <detautama11@gmail.com>
2019-02-13 12:38:39 +0800

    Adding sub-toolbar on media component

--- angular/projects/admin/src/app/admin/media/media.module.ts
+++ angular/projects/admin/src/app/admin/media/media.module.ts
@@ -4,6 +4,7 @@ import { AppMaterialModule } from '../../app-material.module';
 import { AppFirebaseModule } from '../../app-firebase.module';
 import { MediaComponent } from './media.component';
 import { MediaGridComponent } from './media-grid/media-grid.component';
+import { SharedModule } from '../shared.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { MediaGridComponent } from './media-grid/media-grid.component';
     CommonModule,
     AppFirebaseModule,
     AppMaterialModule,
+    SharedModule
   ],
   exports: [
     MediaComponent,
