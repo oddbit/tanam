8e6cb770abebea550d348861c4b9780e4182fad6
Dennis Alund <dennis.alund@gmail.com>
2019-02-21 16:21:08 +0800

    Adding settings page title

--- angular/projects/admin/src/app/admin/settings/settings.module.ts
+++ angular/projects/admin/src/app/admin/settings/settings.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AppFirebaseModule } from '../../app-firebase.module';
 import { AppMaterialModule } from '../../app-material.module';
+import { ComponentsModule } from '../components/components.module';
 import { DomainFormComponent } from './domain-form/domain-form.component';
 import { SettingsComponent } from './settings.component';
 import { SiteFormComponent } from './site-form/site-form.component';
@@ -17,6 +18,7 @@ import { SiteFormComponent } from './site-form/site-form.component';
     AppMaterialModule,
     AppFirebaseModule,
     AppMaterialModule,
+    ComponentsModule,
   ],
   exports: [
     SettingsComponent,
