d9fb92533571090564a50fde8842f9e3165a1448
Dennis Alund <dennis.alund@gmail.com>
2019-02-21 16:14:19 +0800

    Updating breadcrumbs and title for editing document

--- angular/projects/admin/src/app/admin/document/document-edit/document-edit.component.ts
+++ angular/projects/admin/src/app/admin/document/document-edit/document-edit.component.ts
@@ -1,26 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-import { Document } from 'tanam-models';
+import { DocumentTypeService } from '../../../services/document-type.service';
 import { DocumentService } from '../../../services/document.service';
 @Component({
   selector: 'tanam-document-edit',
   templateUrl: './document-edit.component.html',
   styleUrls: ['./document-edit.component.scss']
 })
-export class DocumentEditComponent implements OnInit {
-  document$: Observable<Document>;
+export class DocumentEditComponent {
+  document$ = this.route.paramMap.pipe(switchMap(params => this.documentService.get(params.get('documentId'))));
+  documentType$ = this.document$.pipe(switchMap(document => this.documentTypeService.getDocumentType(document.documentType)));
 
   constructor(
     private readonly route: ActivatedRoute,
     private readonly documentService: DocumentService,
+    private readonly documentTypeService: DocumentTypeService,
   ) { }
-
-  ngOnInit() {
-    this.document$ = this.route.paramMap.pipe(switchMap(params => {
-      const documentTypeId = params.get('documentId');
-      return this.documentService.get(documentTypeId);
-    }));
-  }
 }
