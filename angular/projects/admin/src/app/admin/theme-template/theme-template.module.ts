1f56054317076f40ee84b694b2039d55f496da7e
Dennis Alund <dennis.alund@gmail.com>
2019-02-21 16:34:15 +0800

    Adding page title and breadcrumbs to theme templates

--- angular/projects/admin/src/app/admin/theme-template/theme-template.module.ts
+++ angular/projects/admin/src/app/admin/theme-template/theme-template.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AppFirebaseModule } from '../../app-firebase.module';
 import { AppMaterialModule } from '../../app-material.module';
+import { ComponentsModule } from '../components/components.module';
 import { ThemeTemplateEditComponent } from './theme-template-edit/theme-template-edit.component';
 import { ThemeTemplateFormComponent } from './theme-template-form/theme-template-form.component';
 import { ThemeTemplateListComponent } from './theme-template-list/theme-template-list.component';
@@ -18,6 +19,7 @@ import { ThemeTemplateOverviewComponent } from './theme-template-overview/theme-
     CommonModule,
     AppFirebaseModule,
     AppMaterialModule,
+    ComponentsModule,
   ],
   exports: [
     ThemeTemplateEditComponent,
