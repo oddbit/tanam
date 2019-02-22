e779f5fec3ce0d2d08a69622742ceecddc00ae8e
Dennis Alund <dennis.alund@gmail.com>
2019-02-22 16:19:56 +0800

    Moving "default site info" creation into a service function
    ... instead of trigger

--- functions/src/services/site-info.service.ts
+++ functions/src/services/site-info.service.ts
@@ -5,3 +5,20 @@ export async function getSiteInfo() {
     const siteInfoDoc = await admin.firestore().collection('tanam').doc(process.env.GCLOUD_PROJECT).get();
     return siteInfoDoc.data() as SiteInformation;
 }
+
+export async function createDefaultSiteInfo() {
+    const defaultDomain = `${process.env.GCLOUD_PROJECT}.firebaseapp.com`;
+    console.log(`[createDefaultSiteInfo] ${defaultDomain}`);
+    
+    const siteInfoData: SiteInformation = {
+        defaultLanguage: 'en',
+        languages: ['en'],
+        isCustomDomain: false,
+        domains: [defaultDomain],
+        primaryDomain: defaultDomain,
+        theme: 'default',
+        title: process.env.GCLOUD_PROJECT
+    };
+
+    return admin.firestore().collection('tanam').doc(process.env.GCLOUD_PROJECT).set(siteInfoData);
+}
--- functions/src/triggers/site.ts
+++ functions/src/triggers/site.ts
@@ -1,16 +1,17 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import { SiteInformation } from '../../../models/settings.models';
+import { createDefaultSiteInfo } from '../services/site-info.service';
 
 export const registerHost = functions.database.ref('tanam/{siteId}/domains/{hash}').onCreate(async (snap) => {
     const host = snap.val();
-    console.log(`Discovered request to new host: ${host}`);
+    console.log(`[registerHost] Discovered request to new host: ${host}`);
 
     const siteInfoDoc = admin.firestore().collection('tanam').doc(process.env.GCLOUD_PROJECT);
     const siteIsSetup = (await siteInfoDoc.get()).exists;
     if (!siteIsSetup) {
-        console.log(`Site is not setup yet. Remove this record and retry later when all data is complete.`);
-        return snap.ref.remove();
+        console.log(`[registerHost] Site is not setup yet.`);
+        await createDefaultSiteInfo();
     }
 
     return admin.firestore().runTransaction(async (trx) => {
@@ -23,19 +24,4 @@ export const registerHost = functions.database.ref('tanam/{siteId}/domains/{hash
             trx.update(siteInfoDoc, trxSettings);
         }
     });
-});
-
-export const setupBasicSiteInfo = functions.database.ref('tanam').onCreate(async (snap) => {
-    const defaultDomain = `${process.env.GCLOUD_PROJECT}.firebaseapp.com`;
-    const siteInfoData: SiteInformation = {
-        defaultLanguage: 'en',
-        languages: ['en'],
-        isCustomDomain: false,
-        domains: [defaultDomain],
-        primaryDomain: defaultDomain,
-        theme: '(default)',
-        title: process.env.GCLOUD_PROJECT
-    };
-
-    return admin.firestore().collection('tanam').doc(process.env.GCLOUD_PROJECT).set(siteInfoData);
 });
