5aab29512acb59e9ad39572eadb55985f35adaac
Deta Utama <detautama11@gmail.com>
2019-02-25 12:16:29 +0800

    Removing unused component

--- angular/projects/admin/src/app/admin/settings/settings.module.ts
+++ angular/projects/admin/src/app/admin/settings/settings.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import { AppFirebaseModule } from '../../app-firebase.module';
 import { AppMaterialModule } from '../../app-material.module';
 import { ComponentsModule } from '../components/components.module';
-import { DomainFormComponent } from './domain-form/domain-form.component';
 import { SettingsComponent } from './settings.component';
-import { SiteFormComponent } from './site-form/site-form.component';
 
 @NgModule({
   declarations: [
     SettingsComponent,
-    DomainFormComponent,
-    SiteFormComponent,
   ],
   imports: [
     CommonModule,
