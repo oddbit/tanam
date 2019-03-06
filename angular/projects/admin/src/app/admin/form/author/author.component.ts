38280a8313c3c5afbda2d9c6e42dfd6baacc3e38
Deta Utama <detautama11@gmail.com>
2019-03-06 10:46:46 +0800

    Adding edit author data

--- angular/projects/admin/src/app/admin/form/author/author.component.ts
+++ angular/projects/admin/src/app/admin/form/author/author.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
+interface Author {
+  name: string;
+  profileUrl: string;
+  email: string;
+}
 @Component({
   selector: 'tanam-author',
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.scss']
 })
+
 export class AuthorComponent implements OnInit, OnChanges {
 
   @Input() title: string;
   @Output() changeEvent = new EventEmitter<object>();
+  @Input() author: Author;
 
   authorForm = this.formBuilder.group({
     name: [null, Validators.required],
@@ -22,6 +29,7 @@ export class AuthorComponent implements OnInit, OnChanges {
   ) { }
 
   ngOnInit() {
+    if (this.author) { this.authorForm.patchValue(this.author); }
   }
 
   ngOnChanges() {
