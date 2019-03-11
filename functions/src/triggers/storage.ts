fd292105f6bb1ac0f0d7f24513dd164a5e50dc71
praburangki <praburangki@gmail.com>
2019-03-11 09:42:55 +0800

    move the image model to use locally instead

--- functions/src/triggers/storage.ts
+++ functions/src/triggers/storage.ts
@@ -1,9 +1,42 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as path from 'path';
 import { ObjectMetadata } from 'firebase-functions/lib/providers/storage';
 import * as sharp from 'sharp';
 
-import { ImageData } from '../../../models/image-data.models';
+class ImageData {
+  bucketName: string;
+  filePath: string;
+  baseFileName: string;
+  contentType: string;
+  metadata: any;
+
+  small: string;
+  medium: string;
+  large: string;
+
+  constructor(inputImage: ObjectMetadata) {
+    this.bucketName = inputImage.bucket;
+    this.filePath = inputImage.name;
+    this.baseFileName = path.basename(
+      this.filePath,
+      path.extname(this.filePath),
+    );
+
+    this.contentType = inputImage.contentType;
+    this.metadata = {
+      contentType: 'image/webp',
+      metadata: inputImage.metadata,
+    };
+
+    const [tanamDir, imageDir] = this.filePath.split('/');
+
+    const baseImagePath = path.join(tanamDir, imageDir, this.baseFileName);
+    this.small = `${baseImagePath}_small.webp`;
+    this.medium = `${baseImagePath}_medium.webp`;
+    this.large = `${baseImagePath}_large.webp`;
+  }
+}
 
 export const convertAndResizeImage = functions.storage.object().onFinalize(async (object: ObjectMetadata) => {
   const image = new ImageData(object);
