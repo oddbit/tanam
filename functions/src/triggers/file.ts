4ded1970b1696d2f3f313177e6d2e318d1e5e438
Dennis Alund <dennis.alund@gmail.com>
2019-03-26 17:12:35 +0800

    Adding trigger for deleting cloud storage objects
    ...when their Firestore document  is being deleted.

---
+++ functions/src/triggers/file.ts
@@ -0,0 +1,12 @@
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import { TanamFile } from '../../../models';
+
+export const deleteStorageFiles = functions.firestore.document('tanam/{siteId}/files/{fileId}').onDelete(async (snap) => {
+    const file = snap.data() as TanamFile;
+    const filePrefix = file.filePath.substr(0, file.filePath.lastIndexOf('.'));
+    console.log(`Delete all files starting with ${filePrefix}`);
+    return admin.storage().bucket().deleteFiles({
+        prefix: filePrefix,
+    });
+});
--- functions/src/triggers/index.ts
+++ functions/src/triggers/index.ts
@@ -1,5 +1,6 @@
 export * from './cache';
 export * from './documents';
+export * from './file';
 export * from './site';
 export * from './theme';
 export * from './users';
