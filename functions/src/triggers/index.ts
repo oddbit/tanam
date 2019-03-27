64743dd5b74ba8dbd20c23fd0d35a0e6c4d5bea4
Dennis Alund <dennis.alund@gmail.com>
2019-03-27 11:58:59 +0800

    Adding missing inclusion of storage triggers

--- functions/src/triggers/index.ts
+++ functions/src/triggers/index.ts
@@ -2,5 +2,6 @@ export * from './cache';
 export * from './documents';
 export * from './file';
 export * from './site';
+export * from './storage';
 export * from './theme';
 export * from './users';
