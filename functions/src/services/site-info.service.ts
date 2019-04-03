70156c21dd05953f8c3dcf735340887114311ca4
Dennis Alund <dennis.alund@gmail.com>
2019-04-03 18:13:00 +0800

    Initializing site for first user creation

--- functions/src/services/site-info.service.ts
+++ functions/src/services/site-info.service.ts
@@ -1,5 +1,8 @@
 import * as admin from 'firebase-admin';
 import { SiteInformation } from '../models';
+import { createDefaultDocuments } from '../services/document.service';
+import { createDefaultTemplates } from '../services/template.service';
+import { createDefaultTheme } from '../services/theme.service';
 
 export async function getSiteInfo() {
     const siteInfoDoc = await admin.firestore().collection('tanam').doc(process.env.GCLOUD_PROJECT).get();
@@ -21,4 +24,20 @@ export async function createDefaultSiteInfo() {
     };
 
     return admin.firestore().collection('tanam').doc(process.env.GCLOUD_PROJECT).set(siteInfoData);
+}
+
+export async function initializeSite(force: boolean = false) {
+    const siteInfoDoc = admin.firestore().collection('tanam').doc(process.env.GCLOUD_PROJECT);
+    const siteIsSetup = (await siteInfoDoc.get()).exists;
+    if (siteIsSetup && !force) {
+        return null;
+    }
+
+    console.log(`[registerHost] Site is not setup yet.`);
+    return Promise.all([
+        createDefaultSiteInfo(),
+        createDefaultDocuments(),
+        createDefaultTheme(),
+        createDefaultTemplates(),
+    ]);
 }
--- functions/src/triggers/site.ts
+++ functions/src/triggers/site.ts
@@ -1,28 +1,16 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import { SiteInformation } from '../models/settings.models';
-import { createDefaultDocuments } from '../services/document.service';
-import { createDefaultSiteInfo } from '../services/site-info.service';
-import { createDefaultTemplates } from '../services/template.service';
-import { createDefaultTheme } from '../services/theme.service';
+import { initializeSite } from '../services/site-info.service';
 
 export const registerHost = functions.database.ref('tanam/{siteId}/domains/{hash}').onCreate(async (snap) => {
+    const promises = [];
+
     const host = snap.val();
     console.log(`[registerHost] Discovered request to new host: ${host}`);
 
     const siteInfoDoc = admin.firestore().collection('tanam').doc(process.env.GCLOUD_PROJECT);
-
-    if (!(await siteInfoDoc.get()).exists) {
-        console.log(`[registerHost] Site is not setup yet.`);
-        await Promise.all([
-            createDefaultSiteInfo(),
-            createDefaultDocuments(),
-            createDefaultTheme(),
-            createDefaultTemplates(),
-        ]);
-    }
-
-    return admin.firestore().runTransaction(async (trx) => {
+    promises.push(admin.firestore().runTransaction(async (trx) => {
         const trxDoc = await trx.get(siteInfoDoc);
         const trxSettings = trxDoc.data() as SiteInformation;
         trxSettings.domains = trxSettings.domains || [];
@@ -31,15 +19,14 @@ export const registerHost = functions.database.ref('tanam/{siteId}/domains/{hash
             trxSettings.domains.push(host);
             trx.update(siteInfoDoc, trxSettings);
         }
-    });
+    }));
+
+    return Promise.all(promises);
 });
 
 export const testingSetDefaultData = functions.database.ref('setDefaultData').onCreate(async (snap) => {
     return Promise.all([
-        createDefaultSiteInfo(),
-        createDefaultDocuments(),
-        createDefaultTheme(),
-        createDefaultTemplates(),
+        initializeSite(true),
         snap.ref.remove(),
     ]);
 });
