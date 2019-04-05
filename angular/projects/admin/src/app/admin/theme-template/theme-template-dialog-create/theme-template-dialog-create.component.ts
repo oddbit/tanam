45f779d16a254f3e4f14043486dcba3249f98231
Deta Utama <detautama11@gmail.com>
2019-04-05 12:47:27 +0800

    Adding input for template name and selector name

--- angular/projects/admin/src/app/admin/theme-template/theme-template-dialog-create/theme-template-dialog-create.component.ts
+++ angular/projects/admin/src/app/admin/theme-template/theme-template-dialog-create/theme-template-dialog-create.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { DocumentTypeService } from '../../../services/document-type.service';
+import { DocumentTypeDialogCreateComponent } from '../../document-type/document-type-dialog-create/document-type-dialog-create.component';
+import { Router } from '@angular/router';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'tanam-theme-template-dialog-create',
   templateUrl: './theme-template-dialog-create.component.html',
   styleUrls: ['./theme-template-dialog-create.component.scss']
 })
-export class ThemeTemplateDialogCreateComponent implements OnInit {
+export class ThemeTemplateDialogCreateComponent {
+  readonly createTemplateForm: FormGroup = this.formBuilder.group({
+    title: [null, [Validators.required]],
+    selector: [null, [Validators.required]],
+  });
 
-  constructor() { }
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly documentTypeService: DocumentTypeService,
+    private dialogRef: MatDialogRef<DocumentTypeDialogCreateComponent>,
+    private router: Router,
+  ) { }
 
-  ngOnInit() {
+  createNewTemplate () {
+    alert();
+    console.log(this.createTemplateForm)
   }
-
 }
