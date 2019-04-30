b33452cffce277b5b06a405334f995a8338fb4f6
Deta Utama <detautama11@gmail.com>
2019-04-30 16:29:26 +0800

    Adding dialog list template dialog component

---
+++ angular/projects/admin/src/app/admin/theme-template/theme-template-dialog-list-template/theme-template-dialog-list-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tanam-theme-template-dialog-list-template',
+  templateUrl: './theme-template-dialog-list-template.component.html',
+  styleUrls: ['./theme-template-dialog-list-template.component.scss']
+})
+export class ThemeTemplateDialogListTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
