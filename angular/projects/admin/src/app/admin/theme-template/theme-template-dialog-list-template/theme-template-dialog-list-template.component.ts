9e5d924342f5c7e144a31bb33f2d75073d10c258
Deta Utama <detautama11@gmail.com>
2019-05-01 10:49:36 +0800

    Adding copyTemplate function

--- angular/projects/admin/src/app/admin/theme-template/theme-template-dialog-list-template/theme-template-dialog-list-template.component.ts
+++ angular/projects/admin/src/app/admin/theme-template/theme-template-dialog-list-template/theme-template-dialog-list-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
 import { Observable } from 'rxjs';
 import { ThemeTemplate } from 'tanam-models';
@@ -10,7 +10,7 @@ import { ThemeTemplateService } from '../../../services/theme-template.service';
   templateUrl: './theme-template-dialog-list-template.component.html',
   styleUrls: ['./theme-template-dialog-list-template.component.scss']
 })
-export class ThemeTemplateDialogListTemplateComponent implements OnInit {
+export class ThemeTemplateDialogListTemplateComponent {
 
   readonly templates$: Observable<ThemeTemplate[]> = this.themeTemplateService.getTemplatesForTheme(this.data.themeId);
 
@@ -20,7 +20,29 @@ export class ThemeTemplateDialogListTemplateComponent implements OnInit {
     private snackBar: MatSnackBar
     ) { }
 
-  ngOnInit() {
+  copyTemplate(templateSelector: string) {
+    const txtArea = document.createElement('textarea');
+    txtArea.id = 'txt';
+    txtArea.style.position = 'fixed';
+    txtArea.style.top = '0';
+    txtArea.style.left = '0';
+    txtArea.style.opacity = '0';
+    txtArea.value = '{>"' + templateSelector + '"/}' ;
+    document.body.appendChild(txtArea);
+    txtArea.select();
+
+    try {
+      const successful = document.execCommand('copy');
+      if (successful) {
+        this.snackBar.open('Template Copied', 'Dismiss', {duration: 1000});
+        return true;
+      }
+    } catch (err) {
+      console.log('Oops, unable to copy');
+    } finally {
+      document.body.removeChild(txtArea);
+    }
+    return false;
   }
 
 }
