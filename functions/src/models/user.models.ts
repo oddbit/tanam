867f287c089f1eda79e22f9f746576cb99c1648d
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2019-05-15 11:20:08 +0800

    Change user role to superAdmin and admin

--- functions/src/models/user.models.ts
+++ functions/src/models/user.models.ts
@@ -1,15 +1,15 @@
 
-export type UserRole = 'owner' | 'admin' | 'publisher' | 'designer' | 'reviewer';
+export type UserRole = 'superAdmin' | 'admin' | 'publisher' | 'designer' | 'reviewer';
 
 export interface UserPrefs {
-    theme: string;
-    language: string;
+  theme: string;
+  language: string;
 }
 
 export interface TanamUser {
-    uid: string;
-    name: string;
-    roles: UserRole[];
-    prefs: UserPrefs;
-    photoUrl?: string;
+  uid: string;
+  name: string;
+  roles: UserRole[];
+  prefs: UserPrefs;
+  photoUrl?: string;
 }
