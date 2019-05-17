50bb2072b77606029542acdf6e0c385b91328c33
Deta Utama <detautama11@gmail.com>
2019-05-17 17:01:27 +0800

    Updating total count

--- angular/projects/admin/src/app/admin/document/document-list/document-list.component.ts
+++ angular/projects/admin/src/app/admin/document/document-list/document-list.component.ts
@@ -69,7 +69,7 @@ export class DocumentListComponent implements OnInit, OnDestroy, AfterViewInit {
     this.router.navigateByUrl(url);
   }
   setTotalCount(dataSource: Object) {
-    const refNumEntries = dataSource['documentType'].numEntries;
+    const refNumEntries = dataSource['documentType'].documentCount;
     if (this.status === 'all') {
       this.total_count = refNumEntries.published + refNumEntries.unpublished;
     } else if (this.status === 'published') {
