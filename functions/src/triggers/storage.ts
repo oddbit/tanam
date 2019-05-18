1a34e81d748f6ce3034b96a60402b67390cdd562
Dennis Alund <dennis.alund@gmail.com>
2019-05-18 11:23:34 +0800

    Updating user image upload cloud function
    Remove occurrences of firebase project id for better support of future multi site setup

--- functions/src/triggers/storage.ts
+++ functions/src/triggers/storage.ts
@@ -4,20 +4,22 @@ import { ObjectMetadata } from 'firebase-functions/lib/providers/storage';
 import * as sharp from 'sharp';
 import { TanamFile } from '../models';
 
-export const processImageUpload = functions.storage.object().onFinalize(async (object: ObjectMetadata) => {
-  if (!object.name.startsWith(`tanam/${process.env.GCLOUD_PROJECT}/upload/`)) {
-    console.log(`Not an upload task: ${object.name} (${object.contentType})`);
+export const onUserImageUpload = functions.storage.object().onFinalize(async (storageObject) => {
+  const regexNameMatch = storageObject.name.match(/^\/?tanam\/(.*)\/upload\//);
+  if (!regexNameMatch) {
+    console.log(`Not a user image upload task: ${storageObject.name} (${storageObject.contentType})`);
     return null;
   }
 
-  if (!object.contentType.startsWith('image/')) {
-    console.log(`File is not an image: ${object.name} (${object.contentType})`);
+  if (!storageObject.contentType.startsWith('image/')) {
+    console.log(`File is not an image: ${storageObject.name} (${storageObject.contentType})`);
     return null;
   }
 
-  console.log(`Processing file: ${object.name}`);
-  const bucket = admin.storage().bucket(object.bucket);
-  const [originalFileBuffer] = await bucket.file(object.name).download();
+  console.log(`Processing file: ${storageObject.name}`);
+  const siteId = regexNameMatch[1];
+  const bucket = admin.storage().bucket(storageObject.bucket);
+  const [originalFileBuffer] = await bucket.file(storageObject.name).download();
 
   const resizeAndConvertImage = (size: number) =>
     sharp(originalFileBuffer)
@@ -28,22 +30,25 @@ export const processImageUpload = functions.storage.object().onFinalize(async (o
       .toFormat(sharp.format.webp)
       .toBuffer();
 
-  const originalSuffix = object.name.lastIndexOf('.') > 0 ? object.name.substr(object.name.lastIndexOf('.')) : '';
+  const originalSuffix = storageObject.name.lastIndexOf('.') > 0
+    ? storageObject.name.substr(storageObject.name.lastIndexOf('.'))
+    : '';
+
   const firestoreRef = admin.firestore()
-    .collection('tanam').doc(process.env.GCLOUD_PROJECT)
+    .collection('tanam').doc(siteId)
     .collection('files').doc();
 
   const newFileName = firestoreRef.id;
-  const newFilePath = `tanam/${process.env.GCLOUD_PROJECT}/images/`;
+  const newFilePath = `tanam/${siteId}/images/`;
   const metadata = {
     contentType: 'image/webp',
-    metadata: object.metadata,
+    metadata: storageObject.metadata,
   };
 
   const tanamFile: TanamFile = {
     id: firestoreRef.id,
-    title: object.name.substr(object.name.lastIndexOf('/') + 1),
-    bucket: object.bucket,
+    title: storageObject.name.substr(storageObject.name.lastIndexOf('/') + 1),
+    bucket: storageObject.bucket,
     filePath: [newFilePath, newFileName, originalSuffix].join(''),
     updated: admin.firestore.FieldValue.serverTimestamp(),
     created: admin.firestore.FieldValue.serverTimestamp(),
@@ -53,14 +58,14 @@ export const processImageUpload = functions.storage.object().onFinalize(async (o
       medium: `${newFilePath}${newFileName}_medium.webp`,
       large: `${newFilePath}${newFileName}_large.webp`,
     },
-    mimeType: object.contentType,
+    mimeType: storageObject.contentType,
     fileType: 'image',
   };
 
   return await Promise.all([
     firestoreRef.set(tanamFile),
-    bucket.file(object.name).delete(),
-    bucket.file(tanamFile.filePath).save(originalFileBuffer, object.metadata),
+    bucket.file(storageObject.name).delete(),
+    bucket.file(tanamFile.filePath).save(originalFileBuffer, storageObject.metadata),
     bucket.file(tanamFile.variants.small).save(await resizeAndConvertImage(300), metadata),
     bucket.file(tanamFile.variants.medium).save(await resizeAndConvertImage(800), metadata),
     bucket.file(tanamFile.variants.large).save(await resizeAndConvertImage(1600), metadata),
