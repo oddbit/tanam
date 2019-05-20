06c7d9bd724829fd84dc38f65a6739c897dfe4fd
Dennis Alund <dennis.alund@gmail.com>
2019-05-20 12:38:51 +0800

    Refreshing cache on theme template changes

--- functions/src/triggers/theme.ts
+++ functions/src/triggers/theme.ts
@@ -1,6 +1,7 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import { SiteInformation } from '../models';
+import { Document, SiteInformation } from '../models';
+import * as taskService from '../services/task.service';
 
 export const onUpdateActiveTheme = functions.firestore.document('tanam/{siteId}').onUpdate(async (change) => {
   const siteInfoBefore = change.before.data() as SiteInformation;
@@ -60,6 +61,35 @@ export const onDeleteThemeDeleteAssets = functions.firestore.document('tanam/{si
   batchDeletes.forEach((batchWrite) => {
     promises.push(batchWrite.commit());
   });
+
+  return Promise.all(promises);
+});
+
+export const onWriteTemplateUpdateCache = functions.firestore.document('tanam/{siteId}/themes/{themeId}/templates/{templateId}').onWrite(async (change, context) => {
+  const siteId = context.params.siteId;
+  const themeId = context.params.themeId;
+  const templateId = context.params.themeId;
+  console.log(`Writing to template ${JSON.stringify({ siteId, themeId, templateId })}`)
+
+  const siteInfo = (await admin.firestore().collection('tanam').doc(siteId).get()).data() as SiteInformation;
+  console.log(`Active theme: ${siteInfo.theme}`);
+
+  if (siteInfo.theme !== themeId) {
+    console.log(`Writing template in a theme that is not active. No need to refresh cache.`);
+    return null;
+  }
+
+  const publishedDocs = await admin.firestore()
+    .collection('tanam').doc(siteId)
+    .collection('documents')
+    .where('status', '==', 'published')
+    .get();
+
+  const promises = [];
+  for (const doc of publishedDocs.docs) {
+    const document = doc.data() as Document;
+    promises.push(taskService.updateCache(siteId, document.url));
+  }
 
   return Promise.all(promises);
 });
