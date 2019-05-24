d124651022f4c5df7197da1920d7da145756e716
Deta Utama <detautama11@gmail.com>
2019-05-24 13:39:11 +0800

    Rename updateChache paramter for url

--- angular/projects/admin/src/app/services/task.service.ts
+++ angular/projects/admin/src/app/services/task.service.ts
@@ -9,10 +9,10 @@ export class TaskService {
 
   constructor(private database: AngularFireDatabase, private appConfigService: AppConfigService) { }
 
-  updateCache(documentSlug: string) {
+  updateCache(documentUrl: string) {
     const siteId = this.appConfigService.siteId;
     return this.database
       .list(`tanam/${siteId}/tasks/cache/update`)
-      .push(`${documentSlug}`);
+      .push(`${documentUrl}`);
   }
 }
