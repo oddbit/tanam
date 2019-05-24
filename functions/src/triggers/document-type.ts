12557c97d60a718f3bc6826d35072419d5a2793d
Deta Utama <detautama11@gmail.com>
2019-05-24 15:40:41 +0800

    Adding trigger for standalone changes to documents

--- functions/src/triggers/document-type.ts
+++ functions/src/triggers/document-type.ts
@@ -18,3 +18,26 @@ export const cleanUpDocumentsOfType = functions.firestore.document('/tanam/{site
 
   return Promise.all(promises);
 });
+
+export const updateDocumentsStandaloneValue = functions.firestore.document('/tanam/{siteId}/document-types/{type}').onUpdate(async (snap, context) => {
+  const documentTypeBefore = snap.before.data() as DocumentType;
+  const documentTypeAfter = snap.after.data() as DocumentType;
+  if (documentTypeAfter.standalone === documentTypeBefore.standalone) {
+    console.log('[Update document standalone value] Nothing to update')
+    return null;
+  }
+  console.log(`Updating standalone documents ${documentTypeAfter.id}..`)
+  const querySnaps = await admin.firestore()
+      .collection('tanam').doc(context.params.siteId)
+      .collection('documents').where('documentType', '==', documentTypeAfter.id)
+      .get();
+
+  const promises = [];
+  for (const doc of querySnaps.docs) {
+      promises.push(doc.ref.update({
+        standalone: documentTypeAfter.standalone
+      }));
+  }
+
+  return Promise.all(promises);
+});
