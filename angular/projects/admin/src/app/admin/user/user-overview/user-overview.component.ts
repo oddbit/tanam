275b3f5cad21b6550e4ff712940b6dd85e843cb4
Deta Utama <detautama11@gmail.com>
2019-06-26 16:05:10 +0800

    Adding user overview component

---
+++ angular/projects/admin/src/app/admin/user/user-overview/user-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tanam-user-overview',
+  templateUrl: './user-overview.component.html',
+  styleUrls: ['./user-overview.component.scss']
+})
+export class UserOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angular/projects/admin/src/app/admin/user/user.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { UserOverviewComponent } from './user-overview/user-overview.component';
+
+@NgModule({
+  declarations: [
+    UserOverviewComponent
+  ],
+  imports: [],
+  exports: [UserOverviewComponent],
+})
+export class UserModule { }
