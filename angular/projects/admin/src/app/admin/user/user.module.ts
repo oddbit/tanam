897e5ffb5db07dc4f14ab652fd10b1d5bf80c732
Deta Utama <detautama11@gmail.com>
2019-06-26 17:02:28 +0800

    Adding list item

--- angular/projects/admin/src/app/admin/user/user.module.ts
+++ angular/projects/admin/src/app/admin/user/user.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { UserOverviewComponent } from './user-overview/user-overview.component';
+import { AppMaterialModule } from '../../app-material.module';
 
 @NgModule({
   declarations: [
     UserOverviewComponent
   ],
-  imports: [],
-  exports: [UserOverviewComponent],
+  imports: [
+    AppMaterialModule
+  ],
+  exports: [UserOverviewComponent]
 })
 export class UserModule { }
