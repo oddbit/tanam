51df4ab9b84009ed279427e800134a07953fd394
deta <deta@oddbit.id>
2019-08-15 16:11:46 +0800

    Removing unused comment

--- angular/projects/admin/src/app/admin/form/form.module.ts
+++ angular/projects/admin/src/app/admin/form/form.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
-// import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { MatDatetimepickerModule } from '@mat-datetimepicker/core';
 import { MatMomentDatetimeModule } from '@mat-datetimepicker/moment';
 import { AppMaterialModule } from '../../app-material.module';
