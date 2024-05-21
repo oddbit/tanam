5dcc1fd0db727ec6e417b3376dc2f613b477eb13
Dennis Alund <dennis.alund@gmail.com>
2024-05-21 22:59:25 +0800

    Adding firebase utils

--- hosting/src/firebase.ts
+++ hosting/src/firebase.ts
@@ -1,11 +1,20 @@
 import { getApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 import { useRouter } from "next/router";
 
-const firebaseAppName = useRouter().query.__firebaseAppName;
-const appName = Array.isArray(firebaseAppName)
-  ? firebaseAppName[0]
-  : firebaseAppName;
+export function useFirebase() {
+  const firebaseAppName = useRouter().query.__firebaseAppName;
+  const appName = Array.isArray(firebaseAppName)
+    ? firebaseAppName[0]
+    : firebaseAppName;
 
+  const firebaseApp = getApp(appName);
 
-const firebaseApp = getApp(appName);
-export default firebaseApp;
+  return {
+    auth: getAuth(firebaseApp),
+    firestore: getFirestore(firebaseApp),
+    storage: getStorage(firebaseApp),
+  };
+}
