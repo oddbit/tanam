c5b49d5e1b9d3023be0935f7b32cc4445d71181a
Dennis Alund <dennis.alund@gmail.com>
2024-05-22 15:45:54 +0800

    Loading dynamic data from firestore

--- hosting/src/app/[site]/layout.tsx
+++ hosting/src/app/[site]/layout.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import "jsvectormap/dist/css/jsvectormap.css";
-import "flatpickr/dist/flatpickr.min.css";
+import Loader from "@/components/common/Loader";
 import "@/css/satoshi.css";
 import "@/css/style.css";
-import React, { useEffect, useState } from "react";
-import Loader from "@/components/common/Loader";
 import { useTanamSite } from "@/hooks/useTanamSite";
+import "flatpickr/dist/flatpickr.min.css";
+import "jsvectormap/dist/css/jsvectormap.css";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -14,14 +15,19 @@ interface RootLayoutProps {
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   const [loading, setLoading] = useState<boolean>(true);
-  const { getSite } = useTanamSite("ruang-tempo");
+  const router = useRouter();
+  const pathname = usePathname() ?? "";
+  const site = pathname.split("/")[1];
+  // TODO(Dennis): Fix cases if URL parameter is not a valid site ID
+  // See: https://github.com/oddbit/tanam/issues/347
+  const { getSite } = useTanamSite(site ?? "foo");
 
   useEffect(() => {
     getSite().then((data) => {
       document.title = data?.title ?? "Tanam";
+      setLoading(false);
     });
-    setLoading(false);
-  }, [getSite]);
+  }, [getSite, site, router]);
 
   return (
     <html lang="en">
