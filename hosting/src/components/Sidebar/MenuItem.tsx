c9160489f5ac9901729af16bb7456bbd64d1a031
Dennis Alund <dennis.alund@gmail.com>
2024-05-22 19:49:17 +0800

    Implementing menu item

---
+++ hosting/src/components/Sidebar/MenuItem.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+interface MenuItemProps {
+  href: string;
+  icon: React.ReactNode;
+  title: string;
+}
+
+export function MenuItem({ href, icon, title }: MenuItemProps) {
+  const pathname = usePathname() ?? "/";
+
+  const isActive = pathname.includes(href);
+
+  return (
+    <li>
+      <Link
+        href={href}
+        className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+          isActive && "bg-graydark dark:bg-meta-4"
+        }`}
+      >
+        {icon}
+        {title}
+      </Link>
+    </li>
+  );
+}
