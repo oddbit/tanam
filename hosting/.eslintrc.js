4fb7626d3c3d991fb6357d90bb23be9ead63aef5
Dennis Alund <dennis.alund@gmail.com>
2024-05-24 16:44:04 +0800

    Fixing eslint warning for switch-case-indentations

--- hosting/.eslintrc.js
+++ hosting/.eslintrc.js
@@ -45,7 +45,7 @@ module.exports = {
     "max-len": "off",
     "require-jsdoc": "off",
     "prettier/prettier": "error",
-    indent: ["error", 2],
+    indent: ["error", 2, {SwitchCase: 1}],
     "operator-linebreak": ["error", "before"],
     quotes: "off", // Use config quotes from prettier, so we turn off this rules to avoiding conflict between eslint and prettier
   },
