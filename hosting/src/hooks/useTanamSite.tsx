2ee8e9e9059fb56ce7aea0587a893a0ba581b3b4
Dennis Alund <dennis.alund@gmail.com>
2024-05-24 09:56:07 +0800

    Updating hook

--- hosting/src/hooks/useTanamSite.tsx
+++ hosting/src/hooks/useTanamSite.tsx
@@ -5,39 +5,39 @@ import {TanamSite} from "@/models/tanamSite";
 import {useParams} from "next/navigation";
 
 export function useTanamSite() {
-  const {site} = useParams<{site: string}>() ?? {site: "-"};
+  const {site} = useParams<{site: string}>() ?? {site: null};
   const [siteData, setSiteData] = useState<TanamSite | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    if (!site) {
-      // TODO(Dennis): Redirect to default site
+    if (!!site) {
+      fetchSiteData(site);
+    } else {
+      // TODO(Dennis): Redirect to default site ?
       setError("No site parameter provided");
       return;
     }
-
-    async function fetchSiteData() {
-      try {
-        const docRef = doc(firestore, "tanam", site);
-        const snap = await getDoc(docRef);
-
-        if (snap.exists()) {
-          const data = {
-            ...snap.data(),
-            id: snap.id,
-          };
-          setSiteData(TanamSite.fromJson(data));
-        } else {
-          setError("Site not found");
-        }
-      } catch (err) {
-        console.error("Error fetching site data:", err);
-        setError("No permission to read site data or site not found");
-      }
-    }
-
-    fetchSiteData();
   }, [site]);
+
+  async function fetchSiteData(siteId: string) {
+    try {
+      const docRef = doc(firestore, "tanam", siteId);
+      const snap = await getDoc(docRef);
+
+      if (snap.exists()) {
+        const data = {
+          ...snap.data(),
+          id: snap.id,
+        };
+        setSiteData(TanamSite.fromJson(data));
+      } else {
+        setError("Site not found");
+      }
+    } catch (err) {
+      console.error("Error fetching site data:", err);
+      setError("No permission to read site data or site not found");
+    }
+  }
 
   return {siteData, error};
 }
