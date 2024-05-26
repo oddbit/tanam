c0c9f95397653d98675954e66c61b347337a970e
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-05-26 11:50:53 +0700

    add authaction for session handler

---
+++ hosting/src/actions/authAction.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+import {ROOT_ROUTE, SESSION_COOKIE_NAME} from "@/constants";
+
+export async function createSession(uid: string) {
+  cookies().set(SESSION_COOKIE_NAME, uid, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24, // One day
+    path: "/",
+  });
+
+  redirect(ROOT_ROUTE);
+}
+
+export async function removeSession() {
+  cookies().delete(SESSION_COOKIE_NAME);
+
+  redirect(ROOT_ROUTE);
+}
