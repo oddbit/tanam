6b9c68f020d5d964bb1e151e38e046b4c71a872c
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-05-26 11:42:40 +0700

    add authenticated middleware

---
+++ hosting/src/middleware/authenticated.ts
@@ -0,0 +1,13 @@
+import {type NextRequest, NextResponse} from "next/server";
+import {ROOT_ROUTE, SIGN_IN_ROUTE, SESSION_COOKIE_NAME} from "@/constants"
+
+export const EXCEPT_PAGE = [ROOT_ROUTE]
+
+export default function authenticatedMiddleware(request: NextRequest) {
+  const session = request.cookies.get(SESSION_COOKIE_NAME)?.value || "";
+
+  if (!session && EXCEPT_PAGE.includes(request.nextUrl.pathname)) {
+    const absoluteURL = new URL(SIGN_IN_ROUTE, request.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
+}
