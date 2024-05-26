828f29f12accc7cdb9dea67c100c70db6d069c71
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-05-26 11:51:38 +0700

    dont save sensitive data user in session

--- hosting/src/actions/authAction.ts
+++ hosting/src/actions/authAction.ts
@@ -4,8 +4,8 @@ import {cookies} from "next/headers";
 import {redirect} from "next/navigation";
 import {ROOT_ROUTE, SESSION_COOKIE_NAME} from "@/constants";
 
-export async function createSession(uid: string) {
-  cookies().set(SESSION_COOKIE_NAME, uid, {
+export async function createSession(token: string) {
+  cookies().set(SESSION_COOKIE_NAME, token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     maxAge: 60 * 60 * 24, // One day
