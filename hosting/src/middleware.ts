3a0d664d738a33ced0f024161a3bc9cf49314ced
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-05-26 13:42:20 +0700

    fix logic middleware

--- hosting/src/middleware.ts
+++ hosting/src/middleware.ts
@@ -1,15 +1,21 @@
 import {type NextRequest, NextResponse} from "next/server";
-import {ROOT_ROUTE, SIGN_IN_ROUTE, SESSION_COOKIE_NAME, SIGN_OUT_ROUTE, SIGN_UP_ROUTE} from "@/constants"
+import {ROOT_ROUTE, SIGN_IN_ROUTE, SIGN_UP_ROUTE, SESSION_COOKIE_NAME, SIGN_OUT_ROUTE} from "@/constants"
 
 const EXCEPT_PAGE = [
   ROOT_ROUTE,
   SIGN_IN_ROUTE,
-  SIGN_UP_ROUTE,
-  SIGN_OUT_ROUTE
+  SIGN_UP_ROUTE
 ]
 
 export default function middleware(request: NextRequest) {
   const token = request.cookies.get(SESSION_COOKIE_NAME)?.value || "";
+  // This siteId should be dynamic in the next iteration
+  const siteId = "tanam-testing"
+
+  if (token && EXCEPT_PAGE.includes(request.nextUrl.pathname)) {
+    const absoluteURL = new URL(`${ROOT_ROUTE}${siteId}`, request.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
 
   if (!token && EXCEPT_PAGE.includes(request.nextUrl.pathname)) {
     const absoluteURL = new URL(SIGN_IN_ROUTE, request.nextUrl.origin);
