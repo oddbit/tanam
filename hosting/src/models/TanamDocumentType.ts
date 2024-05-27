e269e86578d55356d171fa86de17e6ba3b8a9dbc
Dennis Alund <dennis.alund@gmail.com>
2024-05-27 16:15:49 +0800

    Updating models

--- hosting/src/models/TanamDocumentType.ts
+++ hosting/src/models/TanamDocumentType.ts
@@ -1,3 +1,5 @@
+import {TanamDocumentField} from "@/models/TanamDocumentField";
+
 /**
  * Tanam document type model.
  */
@@ -5,12 +7,18 @@ export class TanamDocumentType {
   /**
    * Constructor.
    *
-   * @param {string} id document type ID
-   * @param {string} title Document type title
+   * @param {string} id Document type ID
+   * @param {string} titleSingular Document type title in singular
+   * @param {string} titlePlural Document type title in plural
+   * @param {string} documentTitleField The document data field to use as the title
+   * @param {TanamDocumentField[]} fields Array of fields in the document type
    */
   constructor(
     public readonly id: string,
-    public title: string,
+    public titleSingular: string,
+    public titlePlural: string,
+    public documentTitleField: string,
+    public fields: TanamDocumentField[],
   ) {}
 
   /**
@@ -20,18 +28,29 @@ export class TanamDocumentType {
    * @return {TanamDocumentType} Document type instance
    */
   static fromJson(json: any): TanamDocumentType {
-    return new TanamDocumentType(json.id, json.title);
+    const fields: TanamDocumentField[] = json.fields.map((field: any) => TanamDocumentField.fromJson(field));
+
+    return new TanamDocumentType(
+      json.id,
+      json.titleSingular || json.title,
+      json.titlePlural || json.title,
+      json.documentTitleField,
+      fields,
+    );
   }
 
   /**
-   * Serialize to JSON for Firestore
+   * Serialize to JSON for Firestore.
    *
    * @return {any} JSON representation of the document type
    */
   toJson(): any {
     return {
       id: this.id,
-      title: this.title,
+      titleSingular: this.titleSingular,
+      titlePlural: this.titlePlural,
+      documentTitleField: this.documentTitleField,
+      fields: this.fields.map((field) => field.toJson()),
     };
   }
 }
