3cbb881d041ab5149279ffa9e11724eb04e333ea
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-05-27 14:40:37 +0700

    remove pink background in authentication page

--- hosting/src/app/auth/signup/page.tsx
+++ hosting/src/app/auth/signup/page.tsx
@@ -17,7 +17,7 @@ export default function SigninPage() {
         <div className="fixed left-1/2 top-1/2 w-1/3 -translate-x-1/2 -translate-y-1/2 transform text-center">
           <div className="flex justify-center">
             <Image src="/favicon.ico" width={80} height={80} alt="tanam icon" />
-            <h1 className="text-gray-500 mt-10 text-lg font-bold text-white authentication__title">Signup</h1>
+            <h1 className="text-gray-500 mt-10 text-lg font-bold authentication__title">Signup</h1>
           </div>
 
           <div className="authentication__wrapper-content">
