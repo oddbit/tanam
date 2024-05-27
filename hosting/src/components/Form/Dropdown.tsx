57997648d5ff11452aed35d45d059501448f7e7f
Dennis Alund <dennis.alund@gmail.com>
2024-05-27 11:28:08 +0800

    Updating dropdown with new icons

--- hosting/src/components/Form/Dropdown.tsx
+++ hosting/src/components/Form/Dropdown.tsx
@@ -137,22 +137,7 @@ export function Dropdown({
                           onClick={() => !disabled && remove(index)}
                           className="cursor-pointer pl-2 hover:text-danger"
                         >
-                          <svg
-                            className="fill-current"
-                            role="button"
-                            width="12"
-                            height="12"
-                            viewBox="0 0 12 12"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              clipRule="evenodd"
-                              d="M9.35355 3.35355C9.54882 3.15829 9.54882 2.84171 9.35355 2.64645C9.15829 2.45118 8.84171 2.45118 8.64645 2.64645L6 5.29289L3.35355 2.64645C3.15829 2.45118 2.84171 2.45118 2.64645 2.64645C2.45118 2.84171 2.45118 3.15829 2.64645 3.35355L5.29289 6L2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L6 6.70711L8.64645 9.35355C8.84171 9.54882 9.15829 9.54882 9.35355 9.35355C9.54882 9.15829 9.54882 8.84171 9.35355 8.64645L6.70711 6L9.35355 3.35355Z"
-                              fill="currentColor"
-                            ></path>
-                          </svg>
+                          <Image src="/icons/close.svg" alt="Remove" width={12} height={12} />
                         </div>
                       </div>
                     </div>
@@ -174,16 +159,7 @@ export function Dropdown({
                     className="h-6 w-6 cursor-pointer outline-none focus:outline-none"
                     disabled={disabled}
                   >
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <g opacity="0.8">
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-                          fill="#637381"
-                        ></path>
-                      </g>
-                    </svg>
+                    <Image src="/icons/dropdown-arrow.svg" alt="Open" width={24} height={24} />
                   </button>
                 </div>
               </div>
