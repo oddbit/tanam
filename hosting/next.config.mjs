e7da35f3bedb76f2f67182d79dea48ad006d97c0
Dennis Alund <dennis.alund@gmail.com>
2024-05-29 13:36:47 +0800

    Adding static re-routing for /auth/

--- hosting/next.config.mjs
+++ hosting/next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  redirects() {
+    return [
+      {
+        source: "/auth",
+        destination: "/auth/signin",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
