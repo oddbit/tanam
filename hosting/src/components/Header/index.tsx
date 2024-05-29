f0930fa4be9968b83845b866b06c0aaa2133b4ad
Dennis Alund <dennis.alund@gmail.com>
2024-05-29 18:16:17 +0800

    Removing last use of context

--- hosting/src/components/Header/index.tsx
+++ hosting/src/components/Header/index.tsx
@@ -1,14 +1,13 @@
-import Link from "next/link";
 import DarkModeSwitcher from "@/components/Header/DarkModeSwitcher";
 import DropdownMessage from "@/components/Header/DropdownMessage";
 import DropdownNotification from "@/components/Header/DropdownNotification";
 import DropdownUser from "@/components/Header/DropdownUser";
+import {useAuthentication} from "@/hooks/useAuthentication";
 import Image from "next/image";
-import {useAuthUserState} from "@/contexts/AuthUserContext";
+import Link from "next/link";
 
 const Header = (props: {sidebarOpen: string | boolean | undefined; setSidebarOpen: (arg0: boolean) => void}) => {
-  const {state: authState} = useAuthUserState();
-
+  const {authUser} = useAuthentication();
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
@@ -113,10 +112,7 @@ const Header = (props: {sidebarOpen: string | boolean | undefined; setSidebarOpe
           </ul>
 
           {/* <!-- User Area --> */}
-          <DropdownUser
-            displayName={authState.userInfo?.displayName ?? ""}
-            avatar={authState.userInfo?.photoURL ?? ""}
-          />
+          <DropdownUser displayName={authUser?.displayName ?? ""} avatar={authUser?.photoURL ?? ""} />
           {/* <!-- User Area --> */}
         </div>
       </div>
