7ad440858a82f958b34f48dfc022af41f6941be9
Dennis Alund <dennis.alund@gmail.com>
2024-06-06 23:20:34 +0800

    Updating genkit initialization

--- functions/src/genkit/index.ts
+++ functions/src/genkit/index.ts
@@ -3,9 +3,12 @@ import {dotprompt} from "@genkit-ai/dotprompt";
 
 import {firebase} from "@genkit-ai/firebase";
 import {googleAI} from "@genkit-ai/googleai";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 configureGenkit({
-  plugins: [firebase(), googleAI({apiKey: "yourKey"}), dotprompt()],
+  plugins: [firebase(), googleAI({apiKey: process.env.GOOGLE_GENAI_API_KEY}), dotprompt()],
   logLevel: "debug",
   flowStateStore: "firebase",
   traceStore: "firebase",
@@ -13,4 +16,5 @@ configureGenkit({
 });
 
 export * from "./generateArticle";
+export * from "./tweet";
 export * from "./tools";
