b1520a6018f234f9ff22d56e4fbc3c13f139f3b7
WidaSuryani <widasuryani1@gmail.com>
2024-06-08 02:06:34 +0800

    feat : add dependencies icon

--- hosting/tailwind.config.ts
+++ hosting/tailwind.config.ts
@@ -1,5 +1,6 @@
 import type {Config} from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
+import {iconsPlugin, getIconCollections} from "@egoist/tailwindcss-icons";
 
 const config: Config = {
   content: [
@@ -7,6 +8,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   darkMode: "class",
   theme: {
     fontFamily: {
@@ -242,20 +244,20 @@ const config: Config = {
         "card-2": "0px 1px 2px rgba(0, 0, 0, 0.05)",
         switcher: "0px 2px 4px rgba(0, 0, 0, 0.2), inset 0px 2px 2px #FFFFFF, inset 0px -1px 1px rgba(0, 0, 0, 0.1)",
         "switch-1": "0px 0px 5px rgba(0, 0, 0, 0.15)",
-        1: "0px 1px 3px rgba(0, 0, 0, 0.08)",
-        2: "0px 1px 4px rgba(0, 0, 0, 0.12)",
-        3: "0px 1px 5px rgba(0, 0, 0, 0.14)",
-        4: "0px 4px 10px rgba(0, 0, 0, 0.12)",
-        5: "0px 1px 1px rgba(0, 0, 0, 0.15)",
-        6: "0px 3px 15px rgba(0, 0, 0, 0.1)",
-        7: "-5px 0 0 #313D4A, 5px 0 0 #313D4A",
-        8: "1px 0 0 #313D4A, -1px 0 0 #313D4A, 0 1px 0 #313D4A, 0 -1px 0 #313D4A, 0 3px 13px rgb(0 0 0 / 8%)",
-        9: "0px 2px 3px rgba(183, 183, 183, 0.5)",
-        10: "0px 1px 2px 0px rgba(0, 0, 0, 0.10)",
-        11: "0px 1px 3px 0px rgba(166, 175, 195, 0.40)",
-        12: "0px 0.5px 3px 0px rgba(0, 0, 0, 0.18)",
-        13: "0px 1px 3px 0px rgba(0, 0, 0, 0.08)",
-        14: "0px 2px 3px 0px rgba(0, 0, 0, 0.10)",
+        "1": "0px 1px 3px rgba(0, 0, 0, 0.08)",
+        "2": "0px 1px 4px rgba(0, 0, 0, 0.12)",
+        "3": "0px 1px 5px rgba(0, 0, 0, 0.14)",
+        "4": "0px 4px 10px rgba(0, 0, 0, 0.12)",
+        "5": "0px 1px 1px rgba(0, 0, 0, 0.15)",
+        "6": "0px 3px 15px rgba(0, 0, 0, 0.1)",
+        "7": "-5px 0 0 #313D4A, 5px 0 0 #313D4A",
+        "8": "1px 0 0 #313D4A, -1px 0 0 #313D4A, 0 1px 0 #313D4A, 0 -1px 0 #313D4A, 0 3px 13px rgb(0 0 0 / 8%)",
+        "9": "0px 2px 3px rgba(183, 183, 183, 0.5)",
+        "10": "0px 1px 2px 0px rgba(0, 0, 0, 0.10)",
+        "11": "0px 1px 3px 0px rgba(166, 175, 195, 0.40)",
+        "12": "0px 0.5px 3px 0px rgba(0, 0, 0, 0.18)",
+        "13": "0px 1px 3px 0px rgba(0, 0, 0, 0.08)",
+        "14": "0px 2px 3px 0px rgba(0, 0, 0, 0.10)",
       },
       dropShadow: {
         1: "0px 1px 0px #E2E8F0",
@@ -327,6 +329,10 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    iconsPlugin({
+      collections: getIconCollections(["ic", "ri"]),
+    }),
+  ],
 };
 export default config;
