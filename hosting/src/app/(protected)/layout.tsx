eff1715cb2366ca8643381cf61b8fb2f83933625
WidaSuryani <widasuryani1@gmail.com>
2024-06-19 23:03:21 +0800

    fix : fixed bug Signing out doesn't redirect out of the app

--- hosting/src/app/(protected)/layout.tsx
+++ hosting/src/app/(protected)/layout.tsx
@@ -2,11 +2,19 @@
 
 import CmsLayout from "@/components/Layouts/CmsLayout";
 import React from "react";
+import {useAuthentication} from "@/hooks/useAuthentication";
+import {redirect} from "next/navigation";
 
 interface ProtectedLayoutProps {
   children: React.ReactNode;
 }
 
 export default function ProtectedLayout({children}: ProtectedLayoutProps) {
+  const {isSignedIn} = useAuthentication();
+
+  if (isSignedIn === false) {
+    redirect("/auth");
+  }
+
   return <CmsLayout>{children}</CmsLayout>;
 }
