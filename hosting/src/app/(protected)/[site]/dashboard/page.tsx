68c73aec92630210e8b977ebb7902d7166541611
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-06-25 15:43:14 +0700

    using content card

--- hosting/src/app/(protected)/[site]/dashboard/page.tsx
+++ hosting/src/app/(protected)/[site]/dashboard/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 import Notification from "@/components/common/Notification";
+import Loader from "@/components/common/Loader";
+import ContentCard from "@/components/Containers/ContentCard";
 import {useTanamRecentDocuments} from "@/hooks/useTanamDocuments";
 import {useParams} from "next/navigation";
+import {Suspense} from "react";
 
 export default function DashboardPage() {
   const {site} = useParams<{site: string}>() ?? {
@@ -19,11 +22,19 @@ export default function DashboardPage() {
 
   return (
     <>
-      <section className="l-dashboard">
-        site ID :: {site} <br />
-        document :: {JSON.stringify(document)} <br />
-        error :: {JSON.stringify(docError)}
-      </section>
+      {site && (
+        <div className="grid grid-cols-1 gap-9">
+          <ContentCard key={site} title={site}>
+            <Suspense fallback={<Loader />}>
+              <section className="l-dashboard">
+                site ID :: {site} <br />
+                document :: {JSON.stringify(document)} <br />
+                error :: {JSON.stringify(docError)}
+              </section>
+            </Suspense>
+          </ContentCard>
+        </div>
+      )}
     </>
   );
 }
