e3757234f98123d58cee9f4415a79f6f2d3f41e6
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-06-25 15:09:48 +0700

    showing error message

---
+++ hosting/src/app/(protected)/[site]/dashboard/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Notification from "@/components/common/Notification";
+import {useTanamRecentDocuments} from "@/hooks/useTanamDocuments";
+import {useParams} from 'next/navigation';
+
+export default function DashboardPage() {
+  const {site} = useParams<{site: string}>() ?? {
+    site: null,
+  };
+  const {data: document, error: docError} = useTanamRecentDocuments("createdAt");
+
+  if (docError) {
+    return (
+      <>
+        <Notification
+          type="error"
+          title="Error loading document"
+          message={docError?.message || "Unknown error"}
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <section className="l-dashboard">
+        site ID :: {site} <br />
+        document :: {JSON.stringify(document)} <br />
+        error :: {JSON.stringify(docError)}
+      </section>
+    </>
+  );
+}
