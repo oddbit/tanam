286fa4c6b2567d36b399b5df65156e972779047a
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-07-10 15:53:51 +0700

    add syntax highlight codeblock

--- hosting/src/components/Tiptap/TiptapEditor.tsx
+++ hosting/src/components/Tiptap/TiptapEditor.tsx
@@ -1,7 +1,8 @@
 "use client";
-import Loader from "@/components/common/Loader";
 import BubbleMenu from '@/components/Tiptap/BubbleMenu';
+import CodeBlock from '@/components/Tiptap/CodeBlock';
 import FloatingMenu from '@/components/Tiptap/FloatingMenu';
+import Loader from "@/components/common/Loader";
 // import Bold from "@tiptap/extension-bold";
 // import BulletList from "@tiptap/extension-bullet-list";
 // import Document from "@tiptap/extension-document";
@@ -11,13 +12,21 @@ import FloatingMenu from '@/components/Tiptap/FloatingMenu';
 // import OrderedList from "@tiptap/extension-ordered-list";
 // import Paragraph from "@tiptap/extension-paragraph";
 // import Text from "@tiptap/extension-text";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import Underline from "@tiptap/extension-underline";
-import { EditorContent, useEditor } from "@tiptap/react";
-import StarterKit from '@tiptap/starter-kit';
+import { EditorContent, ReactNodeViewRenderer, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import css from "highlight.js/lib/languages/css";
+import js from "highlight.js/lib/languages/javascript";
+import ts from "highlight.js/lib/languages/typescript";
+import html from "highlight.js/lib/languages/xml";
+import { common, createLowlight } from "lowlight";
 import { Suspense, useCallback, useEffect } from "react";
-import "./styles/tiptap-helpers.scss";
+
+import "./styles/tiptap-main.scss";
 
 const DEFAULT_DEBOUNCE = 2000;
+const lowlight = createLowlight(common);
 
 interface TiptapEditorProps {
   key: string;
@@ -44,6 +53,11 @@ function debounce<T extends DebounceFunction>(func: T, wait: number) {
   };
 }
 
+lowlight.register("html", html);
+lowlight.register("css", css);
+lowlight.register("js", js);
+lowlight.register("ts", ts);
+
 export default function TiptapEditor(props: TiptapEditorProps) {
   const debouncedOnChange = useCallback(
     debounce(async (content: string) => {
@@ -59,6 +73,13 @@ export default function TiptapEditor(props: TiptapEditorProps) {
     extensions: [
       StarterKit,
       Underline,
+      CodeBlockLowlight
+        .extend({
+          addNodeView() {
+            return ReactNodeViewRenderer(CodeBlock)
+          },
+        })
+        .configure({ lowlight }),
       // Document,
       // Paragraph,
       // Text,
