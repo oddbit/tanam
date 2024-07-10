3e6acff8e36552a2b7a2011cfbf686df0acf3f32
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-07-10 14:57:38 +0700

    add strike text style

--- hosting/src/components/Tiptap/BubbleMenu.tsx
+++ hosting/src/components/Tiptap/BubbleMenu.tsx
@@ -14,6 +14,7 @@ export default function BubbleMenu({editor}: BubbleMenuProps) {
       <div className="bubble-menu inline-flex h-full leading-none gap-0.5 flex-row p-1 items-center bg-white rounded-lg dark:bg-black shadow-sm border border-neutral-200 dark:border-neutral-800">
         <button className={editor.isActive('bold') ? `${baseStyleButton} is-active` : baseStyleButton} onClick={() => editor.chain().focus().toggleBold().run()}>Bold</button>
         <button className={editor.isActive('italic') ? `${baseStyleButton} is-active` : baseStyleButton} onClick={() => editor.chain().focus().toggleItalic().run()}>Italic</button>
+        <button className={editor.isActive('strike') ? `${baseStyleButton} is-active` : baseStyleButton} onClick={() => editor.chain().focus().toggleStrike().run()}>Strike</button>
       </div>
     </TiptapBubbleMenu>
   ) : (
