273ebc0e75c19ca01234bbf10195cba8ff58ccde
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-07-10 14:51:55 +0700

    fix styling

--- hosting/src/components/Tiptap/BubbleMenu.tsx
+++ hosting/src/components/Tiptap/BubbleMenu.tsx
@@ -7,11 +7,13 @@ interface BubbleMenuProps {
 }
 
 export default function BubbleMenu({editor}: BubbleMenuProps) {
+  const baseStyleButton = 'flex group items-center justify-center text-sm font-semibold rounded-md whitespace-nowrap h-8 gap-1 min-w-[2rem] px-2 w-auto'
+
   return editor ? (
     <TiptapBubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
-      <div className="bubble-menu">
-        <button className={editor.isActive('bold') ? 'is-active' : ''} onClick={() => editor.chain().focus().toggleBold().run()}>Bold</button>
-        <button className={editor.isActive('italic') ? 'is-active' : ''} onClick={() => editor.chain().focus().toggleItalic().run()}>Italic</button>
+      <div className="bubble-menu inline-flex h-full leading-none gap-0.5 flex-row p-1 items-center bg-white rounded-lg dark:bg-black shadow-sm border border-neutral-200 dark:border-neutral-800">
+        <button className={editor.isActive('bold') ? `${baseStyleButton} is-active` : baseStyleButton} onClick={() => editor.chain().focus().toggleBold().run()}>Bold</button>
+        <button className={editor.isActive('italic') ? `${baseStyleButton} is-active` : baseStyleButton} onClick={() => editor.chain().focus().toggleItalic().run()}>Italic</button>
       </div>
     </TiptapBubbleMenu>
   ) : (
