482caa3793de8298698986288a18643e9d0c6e81
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-07-15 14:43:28 +0700

    add separator

--- hosting/src/components/Tiptap/BubbleMenu.tsx
+++ hosting/src/components/Tiptap/BubbleMenu.tsx
@@ -12,6 +12,10 @@ export default function BubbleMenu({editor}: BubbleMenuProps) {
   return editor ? (
     <TiptapBubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
       <div className="bubble-menu inline-flex h-full leading-none gap-0.5 flex-row p-1 items-center bg-white rounded-lg dark:bg-black shadow-sm border border-neutral-200 dark:border-neutral-800">
+        <button className={editor.isActive('strike') ? `${baseStyleButton} is-active` : baseStyleButton} onClick={() => editor.chain().focus().toggleStrike().run()}>Strike</button>
+
+        <span className="separator" />
+        
         <button className={editor.isActive('bold') ? `${baseStyleButton} is-active` : baseStyleButton} onClick={() => editor.chain().focus().toggleBold().run()}>Bold</button>
         <button className={editor.isActive('italic') ? `${baseStyleButton} is-active` : baseStyleButton} onClick={() => editor.chain().focus().toggleItalic().run()}>Italic</button>
         <button className={editor.isActive('underline') ? `${baseStyleButton} is-active` : baseStyleButton} onClick={() => editor.chain().focus().toggleUnderline().run()}>Underline</button>
