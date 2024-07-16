1596ae8c322c2078c8732d58529532923bc17101
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-07-16 15:33:18 +0700

    remove unused

--- hosting/src/components/Tiptap/BubbleMenu.tsx
+++ hosting/src/components/Tiptap/BubbleMenu.tsx
@@ -1,7 +1,6 @@
 import { Editor, BubbleMenu as TiptapBubbleMenu } from "@tiptap/react";
 // import styles from "./BubbleMenu.module.css";
-import { FloatingDropdown } from '@/components/FloatingDropdown';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import "./styles/bubble-menu.scss";
 
 interface BubbleMenuProps {
@@ -14,79 +13,19 @@ export default function BubbleMenu({editor}: BubbleMenuProps) {
 
   const [dropdownFormat, setDropdownFormat] = useState(false);
 
-  const formatButtonRef = useRef<HTMLButtonElement>(null);
-  const alignButtonRef = useRef<HTMLButtonElement>(null);
-
   const toggleDropdownFormat = () => {
     setDropdownFormat(!dropdownFormat);
   }
-
-  const handleButtonClick = (buttonRef: React.RefObject<HTMLButtonElement>) => {
-    console.info('here handleButtonClick :: ', buttonRef)
-    
-    if (formatButtonRef.current?._tippy && alignButtonRef.current?._tippy) {
-      if (buttonRef === formatButtonRef) {
-        alignButtonRef.current._tippy.hide();
-      } else if (buttonRef === alignButtonRef) {
-        formatButtonRef.current._tippy.hide();
-      }
-
-      buttonRef.current?._tippy?.show();
-    }
-  };
 
   return editor ? (
     <TiptapBubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
       <div className="bubble-menu relative inline-flex h-full leading-none gap-0.5 flex-row p-1 items-center bg-white rounded-lg dark:bg-black shadow-sm border border-neutral-200 dark:border-neutral-800">
         <button type="button" className={baseStyleButton} id="menu-button" aria-expanded="true" aria-haspopup="true" onClick={toggleDropdownFormat}>
-          Options
+          Paragraph
           <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
           </svg>
         </button>
-
-        <button
-          ref={formatButtonRef}
-          onClick={() => handleButtonClick(formatButtonRef)}
-          className={baseStyleButton}
-        >
-          Format
-        </button>
-
-        <FloatingDropdown 
-          buttonRef={formatButtonRef}
-          content={(
-            <div className="bg-white p-2 rounded shadow-md space-y-1">
-              <button
-                className="text-gray-700 block px-4 py-2 text-sm w-full text-left hover:bg-gray-100"
-                onClick={() => {
-                  editor.chain().focus().toggleBold().run();
-                  formatButtonRef.current?._tippy?.hide();
-                }}
-              >
-                Bold
-              </button>
-              <button
-                className="text-gray-700 block px-4 py-2 text-sm w-full text-left hover:bg-gray-100"
-                onClick={() => {
-                  editor.chain().focus().toggleItalic().run();
-                  formatButtonRef.current?._tippy?.hide();
-                }}
-              >
-                Italic
-              </button>
-              <button
-                className="text-gray-700 block px-4 py-2 text-sm w-full text-left hover:bg-gray-100"
-                onClick={() => {
-                  editor.chain().focus().toggleUnderline().run();
-                  formatButtonRef.current?._tippy?.hide();
-                }}
-              >
-                Underline
-              </button>
-            </div>
-          )}
-        />
 
         <span className="separator" />
         
