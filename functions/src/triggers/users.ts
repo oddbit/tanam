761ebb6369b3bf1e81501b3b68a02a7bfb8976d1
Dennis Alund <dennis.alund@gmail.com>
2024-07-17 17:22:29 +0200

    Adding trigger methods

---
+++ functions/src/triggers/users.ts
@@ -0,0 +1,76 @@
+import * as admin from "firebase-admin";
+import {Timestamp} from "firebase-admin/firestore";
+import {logger} from "firebase-functions/v2";
+import {onDocumentCreated, onDocumentDeleted, onDocumentUpdated} from "firebase-functions/v2/firestore";
+import {TanamRole} from "../models/TanamUser";
+import {TanamUserAdmin} from "../models/TanamUserAdmin";
+
+const auth = admin.auth();
+const db = admin.firestore();
+
+// Function to validate and assign role on document creation
+// This funciton will scaffold and create a new user document with a role field
+// and assert that all the document fields are populated.
+export const validateAndAssignRole = onDocumentCreated("tanam-users/{docId}", async (event) => {
+  const uid = event.params.docId;
+  const docRef = db.collection("tanam-users").doc(uid);
+  const docData = (await docRef.get()).data() || {};
+
+  logger.info(`Validating User ID: ${uid}`);
+  try {
+    await auth.getUser(uid);
+  } catch (error) {
+    console.log("Document ID does not match any Firebase Auth UID, deleting document");
+    return docRef.delete();
+  }
+
+  const existingDocs = await db.collection("tanam-users").get();
+  const tanamUser = new TanamUserAdmin(uid, {
+    ...docData,
+    role: existingDocs.size === 1 ? "admin" : "publisher",
+    createdAt: Timestamp.now(),
+    updatedAt: Timestamp.now(),
+  });
+  logger.info("Creating User", tanamUser.toJson());
+
+  const customClaims = (await auth.getUser(uid)).customClaims || {};
+  return Promise.all([
+    auth.setCustomUserClaims(uid, {...customClaims, tanamRole: tanamUser.role}),
+    docRef.set(tanamUser.toJson()),
+  ]);
+});
+
+// Function to enforce role management on document update
+// This function will apply changes to custom claims when the role field is updated
+export const enforceRoleManagement = onDocumentUpdated("tanam-users/{docId}", async (event) => {
+  const uid = event.params.docId;
+  const beforeData = event?.data?.before.data();
+  const afterData = event?.data?.after.data();
+  if (!beforeData || !afterData) {
+    throw new Error("Document data is undefined");
+  }
+
+  if (beforeData.role === afterData.role) {
+    logger.debug(`Role unchanged for ${uid}. Stopping here.`);
+    return;
+  }
+
+  const supportedRoles: TanamRole[] = ["admin", "publisher"];
+  if (!supportedRoles.includes(afterData.role)) {
+    logger.error(`Role ${afterData.role} is not supported. Doing nothing.`);
+    return;
+  }
+
+  logger.info(`Role change detected for ${uid}.`, {before: beforeData.role, after: afterData.role});
+  return auth.setCustomUserClaims(uid, {tanamRole: afterData.role});
+});
+
+// Function to remove role on document deletion
+export const removeRoleOnDelete = onDocumentDeleted("tanam-users/{docId}", async (event) => {
+  const uid = event.params.docId;
+
+  console.log(`Document deleted: ${uid}, removing custom claims`);
+  const customClaims = (await auth.getUser(uid)).customClaims || {};
+  customClaims.tanamRole = null;
+  await auth.setCustomUserClaims(uid, customClaims);
+});
