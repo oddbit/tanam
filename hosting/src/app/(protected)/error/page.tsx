7109423adc006f399bdcad3cd872c7ef550f8305
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-07-29 17:01:38 +0700

    update error page

--- hosting/src/app/(protected)/error/page.tsx
+++ hosting/src/app/(protected)/error/page.tsx
@@ -1,8 +1,10 @@
 "use client";
+import Loader from "@/components/common/Loader";
 import Notification from "@/components/common/Notification";
 import PageHeader from "@/components/common/PageHeader";
 import { useAuthentication } from "@/hooks/useAuthentication";
 import { useTanamUser } from "@/hooks/useTanamUser";
+import { Suspense } from "react";
 
 export default function ErrorPage() {
   const {authUser} = useAuthentication();
@@ -10,12 +12,14 @@ export default function ErrorPage() {
 
   return (
     <>
-      <PageHeader pageName="Error" />
-      <Notification
-        type="error"
-        title={userError?.title || "Something wrong"}
-        message={userError?.message || "Unknown error"}
-      />
+      <Suspense fallback={<Loader />}>
+        <PageHeader pageName="Error" />
+        <Notification
+          type="error"
+          title={userError?.title || "Something wrong"}
+          message={userError?.message || "Unknown error"}
+        />
+      </Suspense>
     </>
   );
 }
