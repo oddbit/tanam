827f797d1f845eb9f47ffd8f6e611c1b423e0157
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-07-30 13:28:50 +0700

    specific error page for insufficient role

--- hosting/src/app/(protected)/error/page.tsx
+++ hosting/src/app/(protected)/error/page.tsx
@@ -2,18 +2,14 @@
 import Loader from "@/components/common/Loader";
 import Notification from "@/components/common/Notification";
 import PageHeader from "@/components/common/PageHeader";
-import { Suspense } from "react";
+import {Suspense} from "react";
 
 export default function ErrorPage() {
   return (
     <>
       <Suspense fallback={<Loader />}>
         <PageHeader pageName="Error" />
-        <Notification
-          type="error"
-          title="Something wrong"
-          message="Unknown error"
-        />
+        <Notification type="error" title="Something wrong" message="Unknown error" />
       </Suspense>
     </>
   );
