096b084c7e1c742578e25177374da4f07b287c95
Dennis Alund <dennis.alund@gmail.com>
2024-07-30 10:08:19 +0800

    Updating placeholder avatar

--- hosting/src/components/UserAvatar.tsx
+++ hosting/src/components/UserAvatar.tsx
@@ -1,5 +1,6 @@
 import {useTanamUserImage} from "@/hooks/useTanamUser";
 import Image from "next/image";
+import {Suspense} from "react";
 
 interface UserImageProps {
   uid?: string;
@@ -9,17 +10,26 @@ interface UserImageProps {
 export default function UserAvatar({uid, size = 112}: UserImageProps) {
   const {imageUrl} = useTanamUserImage(uid);
 
-  return imageUrl ? (
-    <Image
-      id={`UserAvatar-${uid}`}
-      width={size}
-      height={size}
-      src={imageUrl}
-      style={{width: "auto", height: "auto"}}
-      alt="User Profile picture"
-    />
-  ) : (
-    // Placeholder Avatar
+  return (
+    <Suspense fallback={<PlaceholderAvatar size={size} />}>
+      {imageUrl ? (
+        <Image
+          id={`UserAvatar-${uid}`}
+          width={size}
+          height={size}
+          src={imageUrl}
+          style={{width: "auto", height: "auto"}}
+          alt="User Profile picture"
+        />
+      ) : (
+        <PlaceholderAvatar size={size} />
+      )}
+    </Suspense>
+  );
+}
+
+function PlaceholderAvatar({size}: {size: number}) {
+  return (
     <div className="flex items-center justify-center">
       <span className="i-ic-baseline-person" style={{fontSize: size}} />
     </div>
