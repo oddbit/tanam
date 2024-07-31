a364e8416404a89885bf6f78f9a80d3b8192d7b7
Dennis Alund <dennis.alund@gmail.com>
2024-07-31 13:55:10 +0800

    Moving header component

--- hosting/src/components/Header.tsx
+++ hosting/src/components/Header.tsx
@@ -6,6 +6,18 @@ interface HeaderProps {
   setSidebarOpen: (open: boolean) => void;
 }
 
+/**
+ * Header Component
+ *
+ * This component renders the header section of the application. It includes:
+ * - A sticky header that remains at the top of the page.
+ * - A toggle button for the sidebar, which is only visible on small screens.
+ * - The toggle button changes the state of the sidebar (open/close) when clicked.
+ * - The header has different styles for light and dark modes.
+ *
+ * @param {HeaderProps} props - The props of the component.
+ * @return {JSX.Element} The header component
+ */
 export default function Header({sidebarOpen, setSidebarOpen}: HeaderProps) {
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
@@ -18,7 +30,7 @@ export default function Header({sidebarOpen, setSidebarOpen}: HeaderProps) {
               setSidebarOpen(!sidebarOpen);
             }}
             className="z-99999 flex items-center justify-center rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
-            style={{width: "40px", height: "40px"}} // Adjust the size as needed
+            style={{width: "40px", height: "40px"}}
           >
             <span className="i-ic-round-menu w-[24px] h-[24px]" />
           </button>
