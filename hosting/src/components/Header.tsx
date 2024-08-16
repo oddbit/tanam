7725b9ff6ac19933f782ccca5d8367dc607ee32b
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-08-16 14:40:15 +0700

    add darkmodeswitcher

--- hosting/src/components/Header.tsx
+++ hosting/src/components/Header.tsx
@@ -1,4 +1,5 @@
-import {TogglePublishDocument} from "@/components/TogglePublishDocument";
+import DarkModeSwitcher from "@/components/DarkModeSwitcher";
+import { TogglePublishDocument } from "@/components/TogglePublishDocument";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -24,6 +25,7 @@ export default function Header({sidebarOpen, setSidebarOpen}: HeaderProps) {
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
         <TogglePublishDocument />
+
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           <button
             aria-controls="sidebar"
@@ -41,6 +43,8 @@ export default function Header({sidebarOpen, setSidebarOpen}: HeaderProps) {
             <Image width={32} height={32} src={"/images/logo.svg"} alt="Logo" />
           </Link>
         </div>
+
+        <DarkModeSwitcher />
       </div>
     </header>
   );
