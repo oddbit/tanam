6811a36e5c65d2870a2f0263a68bf4b54c2c8e76
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-08-16 14:50:17 +0700

    fix structure

--- hosting/src/components/Header.tsx
+++ hosting/src/components/Header.tsx
@@ -24,27 +24,41 @@ export default function Header({sidebarOpen, setSidebarOpen}: HeaderProps) {
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+        {/* Start Toggle Publish Document */}
         <TogglePublishDocument />
+        {/* End Toggle Publish Document */}
 
-        <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
-          <button
-            aria-controls="sidebar"
-            onClick={(e) => {
-              e.stopPropagation();
-              setSidebarOpen(!sidebarOpen);
-            }}
-            className="z-99999 flex items-center justify-center rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
-            style={{width: "40px", height: "40px"}}
-          >
-            <span className="i-ic-round-menu w-[24px] h-[24px]" />
-          </button>
+        {/* Start Hamburger Menu */}
+        <div className="flex items-center gap-2 sm:gap-4">
+          <div className="flex items-center lg:hidden">
+            <button
+              aria-controls="sidebar"
+              onClick={(e) => {
+                e.stopPropagation();
+                setSidebarOpen(!sidebarOpen);
+              }}
+              className="z-99999 flex items-center justify-center rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
+              style={{width: "40px", height: "40px"}}
+            >
+              <span className="i-ic-round-menu w-[24px] h-[24px]" />
+            </button>
 
-          <Link className="block flex-shrink-0 lg:hidden" href="/">
-            <Image width={32} height={32} src={"/images/logo.svg"} alt="Logo" />
-          </Link>
+            <Link className="block flex-shrink-0 pl-4 lg:hidden" href="/">
+              <Image width={32} height={32} src={"/images/logo.svg"} alt="Logo" />
+            </Link>
+          </div>
         </div>
-
-        <DarkModeSwitcher />
+        {/* End Hamburger Menu */}
+        
+        {/* Start List Top Right Menu */}
+        <div className="flex items-center gap-2 2xsm:gap-4">
+          <ul className="flex items-center gap-2 2xsm:gap-4">
+            <li>
+              <DarkModeSwitcher />
+            </li>
+          </ul>
+        </div>
+        {/* End List Top Right Menu */}
       </div>
     </header>
   );
