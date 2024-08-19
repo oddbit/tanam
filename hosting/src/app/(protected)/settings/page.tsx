f7f59efd16c28b2f21da9dfcc18ffb9d9685a160
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-08-19 08:40:43 +0700

    adjust settings screen

--- hosting/src/app/(protected)/settings/page.tsx
+++ hosting/src/app/(protected)/settings/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import PageHeader from "@/components/common/PageHeader";
 import DarkModeSwitcher from "@/components/DarkModeSwitcher";
-import {useAuthentication} from "@/hooks/useAuthentication";
-import {useTanamUser} from "@/hooks/useTanamUser";
+import { useAuthentication } from "@/hooks/useAuthentication";
+import { useTanamUser } from "@/hooks/useTanamUser";
 import Image from "next/image";
 
 export default function Settings() {
@@ -25,46 +25,61 @@ export default function Settings() {
 
         <div className="grid grid-cols-5 gap-8">
           <div className="col-span-5 xl:col-span-3">
+            <form onSubmit={onPersonalInfoSubmit}></form>
             <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
               <div className="border-b border-stroke px-7 py-4 dark:border-strokedark">
                 <h3 className="font-medium text-black dark:text-white">Personal Information</h3>
               </div>
               <div className="p-7">
-                <form onSubmit={onPersonalInfoSubmit}>
-                  <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
-                    <div className="w-full">
-                      <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
-                        Full Name
-                      </label>
-                      <div className="relative">
-                        <span className="absolute left-4 top-3 i-ic-outline-person w-[24px] h-[24px]" />
-                        <input
-                          className="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-                          type="text"
-                          name="fullName"
-                          id="fullName"
-                          placeholder="Your full name"
-                          defaultValue={tanamUser?.name}
-                        />
-                      </div>
+                <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
+                  <div className="w-full">
+                    <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
+                      Full Name
+                    </label>
+                    <div className="relative">
+                      <span className="absolute left-4 top-3 i-ic-outline-person w-[24px] h-[24px]" />
+                      <input
+                        className="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                        type="text"
+                        name="fullName"
+                        id="fullName"
+                        placeholder="Your full name"
+                        defaultValue={tanamUser?.name}
+                      />
                     </div>
                   </div>
+                </div>
+              </div>
+            </div>
 
-                  <div className="flex justify-end gap-4.5">
-                    <button
-                      className="flex justify-center rounded border border-stroke px-6 py-2 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white"
-                      type="submit"
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90"
-                      type="submit"
-                    >
-                      Save
-                    </button>
+            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+              <div className="border-b border-stroke px-7 py-4 dark:border-strokedark">
+                <h3 className="font-medium text-black dark:text-white">System</h3>
+              </div>
+              <div className="p-7">
+                <div className="mb-4">
+                  <label className="mb-3 block w-full text-sm font-medium text-black dark:text-white" htmlFor="emailAddress">
+                    Dark mode
+                  </label>
+                  <div className="relative">
+                    <DarkModeSwitcher />
                   </div>
-                </form>
+                </div>
+
+                <div className="flex justify-end gap-4.5">
+                  <button
+                    className="flex justify-center rounded border border-stroke px-6 py-2 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white"
+                    type="submit"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90"
+                    type="submit"
+                  >
+                    Save
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -123,24 +138,6 @@ export default function Settings() {
                     </button>
                   </div>
                 </form>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-span-5 xl:col-span-2">
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-              <div className="border-b border-stroke px-7 py-4 dark:border-strokedark">
-                <h3 className="font-medium text-black dark:text-white">System</h3>
-              </div>
-              <div className="p-7">
-                <div className="mb-4 flex items-center gap-3">
-                  <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="emailAddress">
-                    Dark mode
-                  </label>
-                  <div className="relative">
-                    <DarkModeSwitcher />
-                  </div>
-                </div>
               </div>
             </div>
           </div>
