b14c285e4c00e2866040fcbb9a127831d9408780
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-08-19 15:31:30 +0700

    add toggle dropzone

--- hosting/src/app/(protected)/settings/page.tsx
+++ hosting/src/app/(protected)/settings/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 import PageHeader from "@/components/common/PageHeader";
 import DarkModeSwitcher from "@/components/DarkModeSwitcher";
-import {useAuthentication} from "@/hooks/useAuthentication";
-import {useTanamUser} from "@/hooks/useTanamUser";
+import { Dropzone } from "@/components/Form/Dropzone";
+import { useAuthentication } from "@/hooks/useAuthentication";
+import { useTanamUser } from "@/hooks/useTanamUser";
 import Image from "next/image";
+import { useState } from 'react';
 
 export default function Settings() {
   const {authUser} = useAuthentication();
   const {tanamUser, saveUserInfo} = useTanamUser(authUser?.uid);
+
+  const [showDropzone, setShowDropzone] = useState(false);
 
   async function onPersonalInfoSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -75,54 +79,39 @@ export default function Settings() {
                 <h3 className="font-medium text-black dark:text-white">Your Photo</h3>
               </div>
               <div className="p-7">
-                <form action="#">
-                  <div className="mb-4 flex items-center gap-3">
-                    <div className="h-14 w-14 rounded-full">
-                      <Image src={"/images/user/user-03.png"} width={55} height={55} alt="User" />
-                    </div>
-                    <div>
-                      <span className="mb-1.5 text-black dark:text-white">Edit your photo</span>
-                      <span className="flex gap-2.5">
-                        <button className="text-sm hover:text-primary">Delete</button>
-                        <button className="text-sm hover:text-primary">Update</button>
-                      </span>
-                    </div>
+                <div className="mb-4 flex items-center gap-3">
+                  <div className="h-14 w-14 rounded-full">
+                    <Image src={"/images/user/user-03.png"} width={55} height={55} alt="User" />
                   </div>
+                  <div>
+                    <span className="mb-1.5 text-black dark:text-white">Edit your photo</span>
+                    <span className="flex gap-2.5">
+                      <button className="text-sm hover:text-primary">Delete</button>
+                      <button className="text-sm hover:text-primary" onClick={() => setShowDropzone(!showDropzone)}>Update</button>
+                    </span>
+                  </div>
+                </div>
 
-                  <div
-                    id="FileUpload"
-                    className="relative mb-5.5 block w-full cursor-pointer appearance-none rounded border border-dashed border-primary bg-gray px-4 py-4 dark:bg-meta-4 sm:py-7.5"
+                {
+                  showDropzone && (
+                    <Dropzone />
+                  )
+                }
+
+                <div className="flex justify-end gap-4.5">
+                  <button
+                    className="flex justify-center rounded border border-stroke px-6 py-2 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white"
+                    type="submit"
                   >
-                    <input
-                      type="file"
-                      accept="image/*"
-                      className="absolute inset-0 z-50 m-0 h-full w-full cursor-pointer p-0 opacity-0 outline-none"
-                    />
-                    <div className="flex flex-col items-center justify-center space-y-3">
-                      <span className="i-ic-outline-file-upload text-primary h-[28px] w-[28px]" />
-                      <p>
-                        <span className="text-primary">Click to upload</span> or drag and drop
-                      </p>
-                      <p className="mt-1.5">SVG, PNG, JPG or GIF</p>
-                      <p>(max, 800 X 800px)</p>
-                    </div>
-                  </div>
-
-                  <div className="flex justify-end gap-4.5">
-                    <button
-                      className="flex justify-center rounded border border-stroke px-6 py-2 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white"
-                      type="submit"
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90"
-                      type="submit"
-                    >
-                      Save
-                    </button>
-                  </div>
-                </form>
+                    Cancel
+                  </button>
+                  <button
+                    className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90"
+                    type="submit"
+                  >
+                    Save
+                  </button>
+                </div>
               </div>
             </div>
           </div>
