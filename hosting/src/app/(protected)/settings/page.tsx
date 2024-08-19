cd3be6cecc7d43f51820a5d0fe62e7768b9ff155
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-08-19 09:56:50 +0700

    adjust settings screen

--- hosting/src/app/(protected)/settings/page.tsx
+++ hosting/src/app/(protected)/settings/page.tsx
@@ -24,102 +24,81 @@ export default function Settings() {
         <PageHeader pageName="Settings" />
 
         <div className="grid grid-cols-5 gap-8">
-          <div className="col-span-5 xl:col-span-3">
-            <form onSubmit={onPersonalInfoSubmit}></form>
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-              <div className="border-b border-stroke px-7 py-4 dark:border-strokedark">
-                <h3 className="font-medium text-black dark:text-white">Personal Information</h3>
+          <div className="col-span-5 xl:col-span-7">
+            <form onSubmit={onPersonalInfoSubmit}>
+              <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                <div className="border-b border-stroke px-7 py-4 dark:border-strokedark">
+                  <h3 className="font-medium text-black dark:text-white">Personal Information</h3>
+                </div>
+                <div className="p-7">
+                  <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
+                    <div className="w-full">
+                      <div className="mb-4 flex items-center gap-3">
+                        <div className="h-14 w-14 rounded-full">
+                          <Image src={"/images/user/user-03.png"} width={55} height={55} alt="User" />
+                        </div>
+                        <div>
+                          <span className="mb-1.5 text-black dark:text-white">Edit your photo</span>
+                          <span className="flex gap-2.5">
+                            <button className="text-sm hover:text-primary">Delete</button>
+                            <button className="text-sm hover:text-primary">Update</button>
+                          </span>
+                        </div>
+                      </div>
+
+                      <div
+                        id="FileUpload"
+                        className="relative mb-5.5 block w-full cursor-pointer appearance-none rounded border border-dashed border-primary bg-gray px-4 py-4 dark:bg-meta-4 sm:py-7.5"
+                      >
+                        <input
+                          type="file"
+                          accept="image/*"
+                          className="absolute inset-0 z-50 m-0 h-full w-full cursor-pointer p-0 opacity-0 outline-none"
+                        />
+                        <div className="flex flex-col items-center justify-center space-y-3">
+                          <span className="i-ic-outline-file-upload text-primary h-[28px] w-[28px]" />
+                          <p>
+                            <span className="text-primary">Click to upload</span> or drag and drop
+                          </p>
+                          <p className="mt-1.5">SVG, PNG, JPG or GIF</p>
+                          <p>(max, 800 X 800px)</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
+                    <div className="w-full">
+                      <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
+                        Full Name
+                      </label>
+                      <div className="relative">
+                        <span className="absolute left-4 top-3 i-ic-outline-person w-[24px] h-[24px]" />
+                        <input
+                          className="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                          type="text"
+                          name="fullName"
+                          id="fullName"
+                          placeholder="Your full name"
+                          defaultValue={tanamUser?.name}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="p-7">
-                <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
-                  <div className="w-full">
-                    <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="fullName">
-                      Full Name
+
+              <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                <div className="border-b border-stroke px-7 py-4 dark:border-strokedark">
+                  <h3 className="font-medium text-black dark:text-white">System</h3>
+                </div>
+                <div className="p-7">
+                  <div className="mb-4">
+                    <label className="mb-3 block w-full text-sm font-medium text-black dark:text-white" htmlFor="emailAddress">
+                      Dark mode
                     </label>
                     <div className="relative">
-                      <span className="absolute left-4 top-3 i-ic-outline-person w-[24px] h-[24px]" />
-                      <input
-                        className="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-                        type="text"
-                        name="fullName"
-                        id="fullName"
-                        placeholder="Your full name"
-                        defaultValue={tanamUser?.name}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-              <div className="border-b border-stroke px-7 py-4 dark:border-strokedark">
-                <h3 className="font-medium text-black dark:text-white">System</h3>
-              </div>
-              <div className="p-7">
-                <div className="mb-4">
-                  <label className="mb-3 block w-full text-sm font-medium text-black dark:text-white" htmlFor="emailAddress">
-                    Dark mode
-                  </label>
-                  <div className="relative">
-                    <DarkModeSwitcher />
-                  </div>
-                </div>
-
-                <div className="flex justify-end gap-4.5">
-                  <button
-                    className="flex justify-center rounded border border-stroke px-6 py-2 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white"
-                    type="submit"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90"
-                    type="submit"
-                  >
-                    Save
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-span-5 xl:col-span-2">
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-              <div className="border-b border-stroke px-7 py-4 dark:border-strokedark">
-                <h3 className="font-medium text-black dark:text-white">Your Photo</h3>
-              </div>
-              <div className="p-7">
-                <form action="#">
-                  <div className="mb-4 flex items-center gap-3">
-                    <div className="h-14 w-14 rounded-full">
-                      <Image src={"/images/user/user-03.png"} width={55} height={55} alt="User" />
-                    </div>
-                    <div>
-                      <span className="mb-1.5 text-black dark:text-white">Edit your photo</span>
-                      <span className="flex gap-2.5">
-                        <button className="text-sm hover:text-primary">Delete</button>
-                        <button className="text-sm hover:text-primary">Update</button>
-                      </span>
-                    </div>
-                  </div>
-
-                  <div
-                    id="FileUpload"
-                    className="relative mb-5.5 block w-full cursor-pointer appearance-none rounded border border-dashed border-primary bg-gray px-4 py-4 dark:bg-meta-4 sm:py-7.5"
-                  >
-                    <input
-                      type="file"
-                      accept="image/*"
-                      className="absolute inset-0 z-50 m-0 h-full w-full cursor-pointer p-0 opacity-0 outline-none"
-                    />
-                    <div className="flex flex-col items-center justify-center space-y-3">
-                      <span className="i-ic-outline-file-upload text-primary h-[28px] w-[28px]" />
-                      <p>
-                        <span className="text-primary">Click to upload</span> or drag and drop
-                      </p>
-                      <p className="mt-1.5">SVG, PNG, JPG or GIF</p>
-                      <p>(max, 800 X 800px)</p>
+                      <DarkModeSwitcher />
                     </div>
                   </div>
 
@@ -137,9 +116,9 @@ export default function Settings() {
                       Save
                     </button>
                   </div>
-                </form>
+                </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
