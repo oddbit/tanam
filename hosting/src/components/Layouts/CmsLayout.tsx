a26433cdefdae013bafb605bca7f9907a844fdaa
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-08-19 11:47:20 +0700

    fix dark/light mode not applied when page is changing

--- hosting/src/components/Layouts/CmsLayout.tsx
+++ hosting/src/components/Layouts/CmsLayout.tsx
@@ -3,12 +3,14 @@ import "@/assets/css/satoshi.css";
 import "@/assets/css/style.css";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
-import {useAuthentication} from "@/hooks/useAuthentication";
+import { useAuthentication } from "@/hooks/useAuthentication";
+import useColorMode from '@/hooks/useColorMode';
 import "flatpickr/dist/flatpickr.min.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function CmsLayout({children}: {children: React.ReactNode}) {
   useAuthentication();
+  useColorMode();
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
