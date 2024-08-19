60c032afdacb1bd9c31adb13dd69f4c54671748d
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-08-19 08:48:39 +0700

    hide top bar in large screen

--- hosting/src/components/Header.tsx
+++ hosting/src/components/Header.tsx
@@ -21,7 +21,7 @@ interface HeaderProps {
  */
 export default function Header({sidebarOpen, setSidebarOpen}: HeaderProps) {
   return (
-    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none lg:hidden">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
         {/* Start Toggle Publish Document */}
         <TogglePublishDocument />
