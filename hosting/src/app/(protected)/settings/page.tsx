dfd4ec9c56e69881ce3bc78312239a4fdfe6c603
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-08-22 11:47:29 +0700

    fix modal action in mobile screen

--- hosting/src/app/(protected)/settings/page.tsx
+++ hosting/src/app/(protected)/settings/page.tsx
@@ -73,6 +73,23 @@ export default function Settings() {
     await saveUserInfo(formData.fullName);
   }
 
+  const modalActionCropImage = (
+    <div className="flex flex-col sm:flex-row justify-end gap-3">
+      <button
+        className="flex justify-center rounded border border-stroke px-6 py-2 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white sm:w-full sm:text-sm"
+        onClick={() => setShowCropImage(false)}
+      >
+        Close
+      </button>
+      <button
+        className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90 sm:w-full sm:text-sm"
+        onClick={() => {}}
+      >
+        Save
+      </button>
+    </div>
+  )
+
   return (
     <>
       <div className="mx-auto max-w-270">
@@ -161,6 +178,7 @@ export default function Settings() {
                           setBeforeCropImage(undefined)
                         }
                       }
+                      actions={modalActionCropImage}
                       title="Crop Profile Picture"
                     >
                       <CropImage 
