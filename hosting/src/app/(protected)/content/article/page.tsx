ae1f21172ce62f8f3ff21ef585be7b1f9bec9d3d
Dennis Alund <dennis.alund@gmail.com>
2024-08-23 14:18:59 +0800

    Implementing usage for genkit creation

--- hosting/src/app/(protected)/content/article/page.tsx
+++ hosting/src/app/(protected)/content/article/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import {Button} from "@/components/Button";
+import Dialog from "@/components/Dialog";
 import {DocumentTypeGenericList} from "@/components/DocumentType/DocumentTypeGenericList";
+import FilePicker from "@/components/FilePicker";
 import Loader from "@/components/common/Loader";
 import Notification from "@/components/common/Notification";
 import PageHeader from "@/components/common/PageHeader";
+import {ArticleCreationStatus, useGenkitArticle} from "@/hooks/useGenkitArticle";
 import {useTanamDocumentType} from "@/hooks/useTanamDocumentTypes";
 import {useCrudTanamDocument, useTanamDocuments} from "@/hooks/useTanamDocuments";
 import {UserNotification} from "@/models/UserNotification";
@@ -16,18 +19,26 @@ export default function DocumentTypeDocumentsPage() {
   const {data: documents, error: docsError} = useTanamDocuments("article");
   const [notification, setNotification] = useState<UserNotification | null>(null);
   const router = useRouter();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const {createFromRecording, status} = useGenkitArticle();
 
   useEffect(() => {
     setNotification(docsError || crudError);
   }, [docsError, crudError]);
 
-  const addNewArticle = async () => {
-    const id = await create(documentType?.id);
-
+  async function addNewArticle() {
+    if (!documentType) return;
+    const id = await create(documentType.id);
     if (!id) return;
-
     router.push(`article/${id}`);
-  };
+  }
+
+  async function handleFileSelect(file: File) {
+    console.log(file);
+    const articleId = await createFromRecording(file);
+    setIsDialogOpen(false);
+    if (articleId) router.push(`article/${articleId}`);
+  }
 
   return (
     <>
@@ -41,12 +52,29 @@ export default function DocumentTypeDocumentsPage() {
                 onClick={addNewArticle}
                 style="rounded"
               />
+              <Button
+                title={`Record new ${documentType.titleSingular.translated}`}
+                onClick={() => setIsDialogOpen(true)}
+                style="rounded"
+              />
             </div>
           </div>
         ) : (
           <Loader />
         )}
       </Suspense>
+      <Dialog isOpen={isDialogOpen} onClose={() => setIsDialogOpen(false)} title={"Tell your story"}>
+        {status === ArticleCreationStatus.Ready ? (
+          <FilePicker onFileSelect={handleFileSelect} />
+        ) : (
+          <div className="flex items-center justify-center">
+            <Loader />
+            {status === ArticleCreationStatus.UploadingFile && <p>Uploading file...</p>}
+            {status === ArticleCreationStatus.GeneratingWithAI && <p>Generating with AI...</p>}
+            {status === ArticleCreationStatus.Finalizing && <p>Finalizing...</p>}
+          </div>
+        )}
+      </Dialog>
 
       {notification && (
         <Notification type={notification.type} title={notification.title} message={notification.message} />
