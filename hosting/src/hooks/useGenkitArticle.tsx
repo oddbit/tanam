80fef08c09f69ad2c2cd8da559042a2fc0820d2c
Dennis Alund <dennis.alund@gmail.com>
2024-08-23 16:45:15 +0800

    Updating hook

--- hosting/src/hooks/useGenkitArticle.tsx
+++ hosting/src/hooks/useGenkitArticle.tsx
@@ -1,47 +1,64 @@
 import {generateArticle} from "@/genkit/article";
+import {ProcessingState} from "@/models/ProcessingState";
 import {TanamDocumentClient} from "@/models/TanamDocumentClient";
 import {UserNotification} from "@/models/UserNotification";
 import {firestore, storage} from "@/plugins/firebase";
 import {TanamPublishStatus} from "@functions/models/TanamDocument";
-import {collection, doc, setDoc} from "firebase/firestore";
+import {ArticleSchema} from "@functions/schemas/article";
+import {collection, doc, getDocs, limit, orderBy, query, setDoc, where} from "firebase/firestore";
 import {getDownloadURL, ref, uploadBytes} from "firebase/storage";
-import {useState} from "react";
-
-export enum ArticleCreationStatus {
-  UploadingFile,
-  GeneratingWithAI,
-  Finalizing,
-  Ready,
-}
+import {useEffect, useState} from "react";
 
 export function useGenkitArticle() {
   const [error, setError] = useState<UserNotification | null>(null);
-  const [status, setStatus] = useState<ArticleCreationStatus | null>(ArticleCreationStatus.Ready);
+  const [status, setStatus] = useState<ProcessingState>(ProcessingState.Ready);
+
+  useEffect(() => {
+    console.log("[useGenkitArticle] " + status);
+  }, [status]);
 
   async function createFromRecording(file: File): Promise<string | null> {
-    const docRef = doc(collection(firestore, "tanam-documents"));
+    const collRef = collection(firestore, "tanam-documents");
+    const docRef = doc(collRef);
     const docId = docRef.id;
 
-    setStatus(ArticleCreationStatus.UploadingFile);
+    setStatus(ProcessingState.Uploading);
     const fileExt = file.name.substring(file.name.lastIndexOf("."));
     const fileName = Date.now().toString();
     const filePath = `tanam-documents/${docId}/GenAI/${fileName}${fileExt}`;
 
     try {
-      console.log("Uploading file: " + filePath);
       const storageRef = ref(storage, filePath);
       await uploadBytes(storageRef, file);
+
+      setStatus(ProcessingState.Processing);
       const downloadURL = await getDownloadURL(storageRef);
 
-      setStatus(ArticleCreationStatus.GeneratingWithAI);
-      const generatedArticle = await generateArticle({recordingUrl: downloadURL, length: 3});
+      const querySnapshot = await getDocs(
+        query(
+          collRef,
+          where("documentType", "==", "article"),
+          where("status", "==", TanamPublishStatus.Published),
+          orderBy("createdAt", "desc"),
+          limit(10),
+        ),
+      );
+      const articles = querySnapshot.docs
+        .map((doc) => TanamDocumentClient.fromFirestore(doc))
+        .map((tanamDoc) => ArticleSchema.parse(tanamDoc.data));
 
-      console.log("Generated article", generatedArticle);
+      setStatus(ProcessingState.Generating);
+      const generatedArticle = await generateArticle({
+        recordingUrl: downloadURL,
+        length: 3,
+        sampleArticles: articles,
+      });
 
-      setStatus(ArticleCreationStatus.Finalizing);
+      setStatus(ProcessingState.Finalizing);
       const tanamDocument = new TanamDocumentClient(docId, {
         documentType: "article",
         status: TanamPublishStatus.Unpublished,
+        publishedAt: undefined,
         data: {
           title: generatedArticle.title,
           content: generatedArticle.content,
@@ -52,6 +69,7 @@ export function useGenkitArticle() {
       await setDoc(docRef, tanamDocument.toJson());
       return docId;
     } catch (err) {
+      console.error(err);
       setError(
         new UserNotification(
           "error",
@@ -60,7 +78,7 @@ export function useGenkitArticle() {
         ),
       );
     } finally {
-      setStatus(ArticleCreationStatus.Ready);
+      setStatus(ProcessingState.Ready);
     }
     return null;
   }
