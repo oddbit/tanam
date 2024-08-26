db87c8c7167a1e302da5ccf55079d8b61fc852ed
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-08-26 14:38:07 +0700

    change button start and stop recording

--- hosting/src/components/VoiceRecorder.tsx
+++ hosting/src/components/VoiceRecorder.tsx
@@ -82,16 +82,28 @@ export function VoiceRecorder(props: VoiceRecorderProps): JSX.Element {
     <div className="max-w-md mx-auto p-4">
       {title && <h2 className="text-xl font-semibold text-center mb-4">{title}</h2>}
       <div className="flex justify-center mb-4">
-        <button
-          onClick={isRecording ? handleStopRecording : handleStartRecording}
-          className={`px-4 py-2 rounded-full text-white ${
-            isRecording ? "bg-red-500 hover:bg-red-600" : "bg-green-500 hover:bg-green-600"
-          } focus:outline-none focus:ring-2 focus:ring-offset-2 ${
-            isRecording ? "focus:ring-red-500" : "focus:ring-green-500"
-          }`}
-        >
-          {isRecording ? "Stop Recording" : "Start Recording"}
-        </button>
+        {isRecording ? (
+          <button
+            onClick={handleStopRecording}
+            className="mt-10 m-auto flex items-center justify-center bg-red-400 hover:bg-red-500 rounded-full w-20 h-20 focus:outline-none"
+          >
+            <svg className="h-12 w-12 " viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path fill="currentColor" d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
+            </svg>
+          </button>
+        ) : (
+          <button
+            onClick={handleStartRecording}
+            className="mt-10 m-auto flex items-center justify-center bg-blue-400 hover:bg-blue-500 rounded-full w-20 h-20 focus:outline-none"
+          >
+            <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg" className="w-12 h-12 text-white">
+              <path
+                fill="currentColor" // Change fill color to the desired color
+                d="M128 176a48.05 48.05 0 0 0 48-48V64a48 48 0 0 0-96 0v64a48.05 48.05 0 0 0 48 48ZM96 64a32 32 0 0 1 64 0v64a32 32 0 0 1-64 0Zm40 143.6V232a8 8 0 0 1-16 0v-24.4A80.11 80.11 0 0 1 48 128a8 8 0 0 1 16 0a64 64 0 0 0 128 0a8 8 0 0 1 16 0a80.11 80.11 0 0 1-72 79.6Z"
+              />
+            </svg>
+          </button>
+        )}
       </div>
       {value && (
         <div className="text-center">
