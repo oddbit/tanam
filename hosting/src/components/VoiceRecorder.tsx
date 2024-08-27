6722a1e32591e05c953b3e6a90b8d77f08886360
Nurfirliana Muzanella <muzanella11@gmail.com>
2024-08-27 11:53:12 +0700

    fix wording

--- hosting/src/components/VoiceRecorder.tsx
+++ hosting/src/components/VoiceRecorder.tsx
@@ -232,20 +232,26 @@ export function VoiceRecorder(props: VoiceRecorderProps): JSX.Element {
               </button>
             </div>
 
-            <h3 className="text-lg font-medium">Recording...</h3>
+            <h3 className="text-lg font-medium">Mic&#39;s Live!</h3>
           </>
         ) : (
-          <button
-            onClick={handleStartRecording}
-            className="mt-10 m-auto flex items-center justify-center bg-blue-400 hover:bg-blue-500 rounded-full w-20 h-20 focus:outline-none"
-          >
-            <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg" className="w-12 h-12 text-white">
-              <path
-                fill="currentColor" // Change fill color to the desired color
-                d="M128 176a48.05 48.05 0 0 0 48-48V64a48 48 0 0 0-96 0v64a48.05 48.05 0 0 0 48 48ZM96 64a32 32 0 0 1 64 0v64a32 32 0 0 1-64 0Zm40 143.6V232a8 8 0 0 1-16 0v-24.4A80.11 80.11 0 0 1 48 128a8 8 0 0 1 16 0a64 64 0 0 0 128 0a8 8 0 0 1 16 0a80.11 80.11 0 0 1-72 79.6Z"
-              />
-            </svg>
-          </button>
+          <>
+            <div className="relative w-full mb-4">
+              <button
+                onClick={handleStartRecording}
+                className="mt-10 m-auto flex items-center justify-center bg-blue-400 hover:bg-blue-500 rounded-full w-20 h-20 focus:outline-none"
+              >
+                <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg" className="w-12 h-12 text-white">
+                  <path
+                    fill="currentColor" // Change fill color to the desired color
+                    d="M128 176a48.05 48.05 0 0 0 48-48V64a48 48 0 0 0-96 0v64a48.05 48.05 0 0 0 48 48ZM96 64a32 32 0 0 1 64 0v64a32 32 0 0 1-64 0Zm40 143.6V232a8 8 0 0 1-16 0v-24.4A80.11 80.11 0 0 1 48 128a8 8 0 0 1 16 0a64 64 0 0 0 128 0a8 8 0 0 1 16 0a80.11 80.11 0 0 1-72 79.6Z"
+                  />
+                </svg>
+              </button>
+            </div>
+
+            <h3 className="text-lg font-medium">{!audioUrl ? "Tap & Speak" : "Tap to Try Again"}</h3>
+          </>
         )}
       </div>
       {audioUrl && (
