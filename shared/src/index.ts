e85cfc9477800ef88d1aa44b11f4b63719ea92aa
Dennis Alund <dennis.alund@gmail.com>
2024-08-27 16:29:08 +0800

    Creating a shared lib package

---
+++ shared/src/index.ts
@@ -0,0 +1,8 @@
+export * from "./definitions/LanguageCode";
+
+export * from "./models/LocalizedString";
+export * from "./models/TanamDocument";
+export * from "./models/TanamDocumentData";
+export * from "./models/TanamDocumentField";
+export * from "./models/TanamDocumentType";
+export * from "./models/TanamUser";
