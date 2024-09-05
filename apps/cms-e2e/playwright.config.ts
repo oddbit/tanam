bf094af70d37c62d2815d062637fbeae70aa1afa
Dennis Alund <dennis.alund@gmail.com>
2024-09-05 15:08:05 +0800

    Renaming dummy app to cms

--- apps/cms-e2e/playwright.config.ts
+++ apps/cms-e2e/playwright.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
+import { defineConfig, devices } from '@playwright/test';
 
 import { workspaceRoot } from '@nx/devkit';
 
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run bazapp:serve-static',
+    command: 'npx nx run cms:serve-static',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
--- apps/cms/src/app/page.tsx
+++ apps/cms/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome bazapp 👋
+              Welcome cms 👋
             </h1>
           </div>
 
@@ -395,7 +395,7 @@ export default function Index() {
                 </svg>
                 View project details
               </summary>
-              <pre>nx show project bazapp --web</pre>
+              <pre>nx show project cms --web</pre>
             </details>
             <details>
               <summary>
