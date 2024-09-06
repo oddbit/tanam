657f8cdbea98d4014def46cc8793f66373201611
Dennis Alund <dennis.alund@gmail.com>
2024-09-06 15:58:29 +0800

    Removing unused code

--- apps/cms/src/models/UserNotification.ts
+++
@@ -1,9 +0,0 @@
-type NotificationType = "warning" | "success" | "error";
-
-export class UserNotification {
-  constructor(
-    public readonly type: NotificationType,
-    public readonly title: string,
-    public readonly message: string,
-  ) {}
-}
