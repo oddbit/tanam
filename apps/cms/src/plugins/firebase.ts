220616e7c7311245eda2d1b43902d55cc70a591a
Dennis Alund <dennis.alund@gmail.com>
2024-09-23 16:00:11 +0800

    Adding measurement ID to config

--- apps/cms/src/plugins/firebase.ts
+++ apps/cms/src/plugins/firebase.ts
@@ -11,6 +11,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
