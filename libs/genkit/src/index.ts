f1c8f460b75011ee3df49964fdbf77472a953027
Dennis Alund <dennis.alund@gmail.com>
2024-09-27 09:57:40 +0800

    Updating genkit lib

--- libs/genkit/src/index.ts
+++ libs/genkit/src/index.ts
@@ -1,11 +1,7 @@
-"use server";
 import {configureGenkit} from "@genkit-ai/core";
 import {firebase} from "@genkit-ai/firebase";
-import {runFlow} from "@genkit-ai/flow";
 import googleAI from "@genkit-ai/googleai";
 import * as dotenv from "dotenv";
-import {z} from "zod";
-import {articleFlow, PromptInputSchema} from "./article";
 
 if (process.env.GENKIT_ENV === "dev" || process.env.NODE_ENV === "development") {
   // Load environment variables for local development
@@ -26,6 +22,4 @@ configureGenkit({
   logLevel: "info",
 });
 
-export async function generateArticle(input: z.infer<typeof PromptInputSchema>) {
-  return runFlow(articleFlow, input);
-}
+export * from "./article";
